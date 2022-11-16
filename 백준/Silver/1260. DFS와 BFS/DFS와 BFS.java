5bfacf6de574d671c67be6e60c758d72ba7b73f8
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2022-11-16 09:33:07 +0900

    [Silver II] Title: DFS와 BFS, Time: 624 ms, Memory: 42700 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,10 +4,12 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
+	
 	static int vertex;
 	static int edge;
 	static int[][] graph;
 	static boolean[] check;
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
@@ -16,17 +18,19 @@ public class Main {
 		int start = scan.nextInt();
 		
 		graph = new int[vertex+1][vertex+1];
-		check = new boolean[vertex+1];
+		
 		for(int i = 1; i <= edge; i++) {
-			int v = scan.nextInt();
-			int e = scan.nextInt();
+			int v1 = scan.nextInt();
+			int v2 = scan.nextInt();
 			
-			graph[v][e] = graph[e][v] = 1;
+			graph[v1][v2] = graph[v2][v1] = 1;
 		}
 		
-		dfs(start);
 		check = new boolean[vertex+1];
+		
+		dfs(start);
 		System.out.println();
+		check = new boolean[vertex+1];
 		bfs(start);
 	}
 	
@@ -34,29 +38,30 @@ public class Main {
 		check[start] = true;
 		System.out.print(start + " ");
 		
-		
-		for(int i = 1; i <= vertex; i++) {
+		for(int i = 1; i <=vertex; i++) {
 			if(graph[start][i] == 1 && !check[i]) {
 				dfs(i);
 			}
 		}
-		
 	}
 	
 	public static void bfs(int start) {
-		Queue<Integer> q  = new LinkedList<>();
-		check[start] = true;
+		Queue<Integer> q = new LinkedList<>();
+		
 		q.add(start);
+		check[start] = true;
 		
 		while(!q.isEmpty()) {
 			int v = q.poll();
 			System.out.print(v + " ");
+			
 			for(int i = 1; i <= vertex; i++) {
 				if(graph[v][i] == 1 && !check[i]) {
+					q.offer(i);
 					check[i] = true;
-					q.add(i);
 				}
 			}
 		}
 	}
+
 }
