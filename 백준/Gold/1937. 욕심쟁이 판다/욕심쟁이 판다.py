3e9bb56d911b803c56ab62459dbc44f146c5e4f6
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-01-28 00:08:29 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 1020 ms, Memory: 52012 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,31 +1,42 @@
 import sys
-
-input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
 
+N = int(sys.stdin.readline())
+
+graph = []
+dp = [[0] * N for _ in range(N)]
+
+for _ in range(N):
+    graph.append(list(map(int, sys.stdin.readline().split())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def dfs(x, y):
-    if dp[x][y]: return dp[x][y] # 이미 한번 왔다간 경로는 그대로 리턴
+    if dp[x][y]:
+        return dp[x][y]
     dp[x][y] = 1
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n and arr[x][y] < arr[nx][ny]:
+
+        if 0 <= nx < N and 0 <= ny < N and graph[x][y] < graph[nx][ny]:
             dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1)
+
     return dp[x][y]
 
+maxValue = 0
+for i in range(N):
+    for j in range(N):
+        maxValue = max(maxValue, dfs(i, j))
 
-n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
-dx = [-1, 0, 0, 1]
-dy = [0, -1, 1, 0]
-dp = [[0] * n for _ in range(n)]
+print(maxValue)
 
-answer = 0
-for i in range(n):
-    for j in range(n):
-        answer = max(answer, dfs(i, j))
 
-print(answer)
+
+
+
+
+
+
