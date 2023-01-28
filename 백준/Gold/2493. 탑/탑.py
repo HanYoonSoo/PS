48e5de28c25903b9b8d794c67e56f3c483d0cc91
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-01-28 11:55:49 +0900

    [Gold V] Title: 탑, Time: 524 ms, Memory: 160712 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+top = list(map(int, input().split()))
+stack = []
+result = []
+
+for i in range(N):
+    while stack:
+        if stack[-1][1] > top[i]:
+            result.append(stack[-1][0] + 1)
+            break
+        else:
+            stack.pop()
+
+    if not stack:
+        result.append(0)
+    stack.append((i, top[i]))
+
+print(' '.join(map(str, result)))
