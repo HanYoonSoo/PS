3ea69bb83100805dd7aea228b2ace9c84fadfdd7
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-04 20:25:59 +0900

    [Gold III] Title: 소풍, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2026. 소풍/소풍.py
+++ 백준/Gold/2026. 소풍/소풍.py
@@ -1,53 +1,46 @@
 import sys
 
-
-def dfs(start):
-    global visited, check, compare
-    if compare:
-        return
-    visited[start] = True
-    check.append(start)
-
-    if K == len(check):
-
-        for i in range(len(check)):
-            print(check[i])
-        compare = True
-        return
-    else:
-        for i in range(start + 1, N+1):
-            if not visited[i]:
-                flag = True
-                for j in check:
-                    if not graph[i][j]:
-                        flag = False
-                        break
-
-                if flag:
-                    dfs(i)
-                    if len(check) != 0:
-                        check.pop()
-
-
-
 K, N, F = map(int, sys.stdin.readline().split())
 
-graph = [[False] * (N+1) for _ in range(N+1)]
+graph = [[] for _ in range(N+1)]
 
 for _ in range(F):
-    v1, v2 = map(int, sys.stdin.readline().split())
-    graph[v1][v2] = True
-    graph[v2][v1] = True
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
+for i in range(1, N):
+    graph[i].sort()
 
+def dfs(x, result, visited):
+    visited[x] = True
+    result.append(x)
+
+    if len(result) == K:
+        if startV in graph[x]:
+            for v in result:
+                print(v)
+            exit(0)
+        return
+    for v in graph[x]:
+        if not visited[v]:
+            compare = True
+
+            for j in result:
+                if v not in graph[j]:
+                    compare = False
+                    break
+
+            if compare:
+                dfs(v, result, visited)
+                if len(result) != 0:
+                    result.pop()
 
 for i in range(1, N+1):
-    compare = False
-    check = []
-    visited = [False] * (N + 1)
+    visited = [False] * (N+1)
+    result = []
+    if not visited[i]:
+        startV = i
+        dfs(i, result, visited)
 
-    dfs(i)
-    if compare:
-        break
-if not compare:
-    print(-1)
+print(-1)
