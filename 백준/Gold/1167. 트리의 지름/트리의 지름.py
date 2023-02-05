e6db601fd387bc3414ed15ef06db24e0221f0aab
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-05 17:07:27 +0900

    [Gold II] Title: 트리의 지름, Time: 644 ms, Memory: 71492 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,40 +1,33 @@
 import sys
 
+def dfs(start, w):
+    for v, v_w in graph[start]:
+        if distance[v] == -1:
+            distance[v] = w + v_w
+            dfs(v, distance[v])
+
 V = int(sys.stdin.readline())
 
-def dfs(start , w):
-    for v_i, v_w in graph[start]:
-        if distance[v_i] == -1:
-            distance[v_i] = v_w + w
-            dfs(v_i, distance[v_i])
-
-
-graph = [[] * (V+1) for _ in range(V+1)]
+graph = [[] for _ in range(V+1)]
 
 for _ in range(V):
-    temp = list(map(int, sys.stdin.readline().split()))
+    temp = list(map(int, sys.stdin.readline().split()))[:-1]
+
     v = temp[0]
-    temp.pop(0)
-    while temp[0] != -1:
-        graph[v].append((temp[0], temp[1]))
-        temp.pop(0)
-        temp.pop(0)
+
+    for i in range(1, len(temp), 2):
+        graph[v].append((temp[i], temp[i+1]))
 
 distance = [-1] * (V+1)
 distance[1] = 0
 
 dfs(1, 0)
 
-maxi = distance.index(max(distance))
+maxIdx = distance.index(max(distance))
 
-distance = [-1] * (V + 1)
-distance[maxi] = 0
+distance = [-1] * (V+1)
+distance[maxIdx] = 0
 
-dfs(maxi, 0)
-
+dfs(maxIdx, 0)
 print(max(distance))
-
-
-
-
 
