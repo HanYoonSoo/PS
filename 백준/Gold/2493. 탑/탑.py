33caf1276331f4f2f0ac6dc87f46c02cb1f7fabf
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-08 16:28:39 +0900

    [Gold V] Title: 탑, Time: 520 ms, Memory: 146380 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,19 +1,23 @@
-N = int(input())
+import sys
 
-top = list(map(int, input().split()))
+N = int(sys.stdin.readline())
+
+top = list(map(int, sys.stdin.readline().split()))
+
 stack = []
 result = []
 
 for i in range(N):
     while stack:
-        if stack[-1][1] > top[i]:
-            result.append(stack[-1][0] + 1)
-            break
-        else:
+        if stack[-1][1] < top[i]:
             stack.pop()
+        else:
+            result.append(stack[-1][0])
+            break
 
     if not stack:
         result.append(0)
-    stack.append((i, top[i]))
+    stack.append(((i+1),top[i]))
 
-print(' '.join(map(str, result)))
+print(' '.join(map(str,result)))
+
