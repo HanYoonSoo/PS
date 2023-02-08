183856a7928d4f5eafa844c87890fd82d17d6059
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-08 15:04:49 +0900

    [Gold III] Title: 순회강연, Time: 68 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.py
+++ 백준/Gold/2109. 순회강연/순회강연.py
@@ -1,15 +1,22 @@
+import sys
 import heapq
 
-N=int(input())
-col=[]
-for i in range(N):
-  col.append(list(map(int, input().split())))
+N = int(sys.stdin.readline())
 
-col.sort(key=lambda x: x[1])
-minHeap=[]
-for i in col:
-  heapq.heappush(minHeap, i[0])
-  if (len(minHeap)>i[1]):
-    heapq.heappop(minHeap)
+course = []
+
+for _ in range(N):
+    p, d = map(int, sys.stdin.readline().split())
+
+    course.append([p, d])
+
+course.sort(key=lambda x : x[1])
+
+minHeap = []
+for c in course:
+    heapq.heappush(minHeap, c[0])
+
+    if len(minHeap) > c[1]:
+        heapq.heappop(minHeap)
 
 print(sum(minHeap))
