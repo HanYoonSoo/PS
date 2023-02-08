428ab161f52dbb96c2478df5fc35d9165fbdd62d
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-08 13:56:54 +0900

    [Gold V] Title: 도서관, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -7,31 +7,31 @@ arr = list(map(int, sys.stdin.readline().split()))
 plus = []
 minus = []
 
-for i in arr:
-    if i < 0:
-        minus.append(i)
-    elif i > 0:
-        plus.append(i)
-
-distance = []
-minus.sort()
-
-for i in range(len(minus) // M):
-    distance.append(abs(minus[M*i]))
-if len(minus) % M > 0:
-    distance.append(abs(minus[(len(minus)//M) * M]))
+for i in range(N):
+    if arr[i] > 0:
+        plus.append(arr[i])
+    else:
+        minus.append(arr[i])
 
 plus.sort(reverse=True)
+minus.sort()
+
+distance = []
 
 for i in range(len(plus) // M):
-    distance.append(abs(plus[M*i]))
+    distance.append(plus[i * M])
 if len(plus) % M > 0:
-    distance.append(abs(plus[(len(plus)//M) * M]))
+    distance.append(plus[len(plus) // M * M])
+
+for i in range(len(minus) // M):
+    distance.append(abs(minus[M * i]))
+if len(minus) % M > 0:
+    distance.append(abs(minus[len(minus) // M * M]))
 
 distance.sort()
 
 result = distance.pop()
 
 result += 2 * sum(distance)
-print(result)
 
+print(result)
