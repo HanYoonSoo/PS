d1574024c4948fd5c186ed8ffca33f94fe18f256
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-08 20:20:31 +0900

    [Gold III] Title: 앱, Time: 692 ms, Memory: 48032 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.py
@@ -0,0 +1,24 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+m = [0] + list(map(int, sys.stdin.readline().split()))
+c = [0] + list(map(int, sys.stdin.readline().split()))
+
+dp = [[0] * (sum(c) + 1) for _ in range(N+1)]
+
+result = sum(c)
+
+for i in range(1, N+1):
+    for j in range(1, sum(c)):
+        if c[i] > j:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-c[i]] + m[i])
+
+        if dp[i][j] >= M:
+            result = min(result, j)
+
+print(result)
+
+
