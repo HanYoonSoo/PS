39f17af586e1fc0baaf2b69ab1ec9409877926e3
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-08 17:43:08 +0900

    [Gold I] Title: 최소 환승 경로, Time: 1948 ms, Memory: 171372 KB -BaekjoonHub

--- 백준/Gold/2021. 최소 환승 경로/최소 환승 경로.py
+++ 백준/Gold/2021. 최소 환승 경로/최소 환승 경로.py
@@ -1,37 +1,48 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
-N, L = map(int, input().split())
-station = [list() for _ in range(N+1)]
-line = [list() for _ in range(L)]
-for i in range(L):
-    tmp = list(map(int, input().split()))
-    for j in range(len(tmp)):
-        if tmp[j] == -1: break
-        station[tmp[j]].append(i)    # 역에 연결되어있는 라인
-        line[i].append(tmp[j])  # 라인에 연결되어있는 역
 
-START, END = map(int, input().split())
-def bfs(START, END):
-    if START == END: return 0
-    visit = [-1 for _ in range(N + 1)]
-    visit_line = [False for _ in range(L)]
+N, L = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(N+1)]
+line = [[] for _ in range(L)]
+
+for i in range(L):
+    temp = list(map(int, sys.stdin.readline().split()))[:-1]
+    for j in range(len(temp)):
+        graph[temp[j]].append(i)
+        line[i].append(temp[j])
+
+def bfs(start, end):
+    if start == end:
+        return 0
+
     q = deque()
-    visit[START] = 0
-    for tmp_line in station[START]:
-        q.append((START, tmp_line))
-        visit_line[tmp_line] = True
+
+    visit = [-1] * (N+1)
+    visit_line = [False] * L
+
+    visit[start] = 0
+
+    for temp_line in graph[start]:
+        q.append((start, temp_line))
+        visit_line[temp_line] = True
+
     while q:
         cur_node, cur_line = q.popleft()
-        # print(cur_node, cur_line)
-        for adj_node in line[cur_line]:
-            if visit[adj_node] != -1: continue
-            if adj_node == END:
-                return visit[cur_node]
-            visit[adj_node] = visit[cur_node] + 1
-            for connected_line in station[adj_node]:
-                if visit_line[connected_line]: continue
-                q.append((adj_node, connected_line))
+
+        for node in line[cur_line]:
+            if visit[node] == -1:
+                if node == end:
+                    return visit[cur_node]
+                visit[node] = visit[cur_node] + 1
+
+                for connected_line in graph[node]:
+                    if not visit_line[connected_line]:
+                        q.append((node, connected_line))
+
     return -1
 
-print(bfs(START,END))
+start, end = map(int, sys.stdin.readline().split())
+
+print(bfs(start, end))
+
