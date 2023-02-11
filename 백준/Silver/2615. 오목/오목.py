2d6e517bd2b7664d6ee3739c5d95695145fa9eed
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-11 22:23:33 +0900

    [Silver I] Title: 오목, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,38 +1,45 @@
-n = 19
-arr = [list(map(int, input().split())) for _ in range(n)]
- 
-dx = [1, 1, 0, -1]  # 하(↓), 우하(⬊), 우(➞), 우상(⬈)
-dy = [0, 1, 1, 1]    
- 
-def omok():
-    for x in range(n):
-        for y in range(n):
-            if arr[x][y]:
-                for i in range(4):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    cnt = 1
- 
-                    if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                        continue
- 
-                    while 0 <= nx < n and 0 <= ny < n and arr[x][y] == arr[nx][ny]:
-                        cnt += 1
- 
-                        if cnt == 5:
-                            if 0 <= nx + dx[i] < n and 0 <= ny + dy[i] < n and arr[nx][ny] == arr[nx + dx[i]][ny + dy[i]]:  # 육목 판정 1
-                                break
-                            if 0 <= x - dx[i] < n and 0 <= y - dy[i] < n and arr[x][y] == arr[x - dx[i]][y - dy[i]]:  # 육목 판정 2
-                                break
-                            return arr[x][y], x+1, y+1  # 육목이 아닌 오목이면 return
- 
-                        nx += dx[i]
-                        ny += dy[i]
-    return 0, -1, -1  # 승부가 나지 않을 때
- 
-color, x, y = omok()
-if not color:
-    print(color)
-else:
-    print(color)
-    print(x, y)
+import sys
+
+input = sys.stdin.readline
+
+graph = []
+
+for _ in range(19):
+    graph.append(list(map(int, input().split())))
+
+def bfs(a, b):
+    dx = [1, 1, 0, -1]  # 하(↓), 우하(⬊), 우(➞), 우상(⬈)
+    dy = [0, 1, 1, 1]
+    for i in range(4):
+        count = 1
+        x = a
+        y = b
+        while 0 <= x + dx[i] < 19 and 0 <= y + dy[i] < 19 and graph[x + dx[i]][y + dy[i]] == graph[a][b]:
+            x = x + dx[i]
+            y = y + dy[i]
+
+            count += 1
+
+            # print(x,y,count)
+
+        if count == 5:
+            if 0 <= x + dx[i] < 19 and 0 <= y + dy[i] < 19 and graph[x + dx[i]][y + dy[i]] == graph[x][y]:
+                break
+            if 0 <= a - dx[i] < 19 and 0 <= b - dy[i] < 19 and graph[a - dx[i]][b - dy[i]] == graph[a][b]:
+                break
+
+            return True
+
+    return False
+
+
+
+for i in range(19):
+    for j in range(19):
+        if graph[i][j] == 1 or graph[i][j] == 2:
+            if bfs(i, j):
+                print(graph[i][j])
+                print(i+1, j+1)
+                exit(0)
+
+print(0)
