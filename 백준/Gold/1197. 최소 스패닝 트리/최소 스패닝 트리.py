ee80511a34902763400b9c7a21072a0f7c721184
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-13 20:35:49 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 492 ms, Memory: 59312 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,27 +1,35 @@
 import sys
 import heapq
+
 input = sys.stdin.readline
- 
+
 V, E = map(int, input().split())
-visited = [False]*(V+1)
-Elist = [[] for _ in range(V+1)]
-heap = [[0, 1]]
+
+graph = [[] for _ in range(V+1)]
+
 for _ in range(E):
-    s, e, w = map(int, input().split())
-    Elist[s].append([w, e])
-    Elist[e].append([w, s])
- 
-answer = 0
-cnt = 0
-while heap:
-    if cnt == V:
-        break
-    w, s = heapq.heappop(heap)
-    if not visited[s]:
-        visited[s] = True
-        answer += w
-        cnt += 1
-        for i in Elist[s]:
-            heapq.heappush(heap, i)
- 
-print(answer)
+    a, b, w = map(int, input().split())
+    graph[a].append((w, b))
+    graph[b].append((w, a))
+
+def prim():
+    visited = [False] * (V+1)
+    heap = [[0, 1]]
+    count = 0
+    result = 0
+
+    while heap:
+        if count == V:
+            return result
+
+        w, vertex = heapq.heappop(heap)
+
+        if not visited[vertex]:
+            visited[vertex] = True
+            count += 1
+            result += w
+            for info in graph[vertex]:
+                heapq.heappush(heap, info)
+
+
+print(prim())
