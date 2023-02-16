a3677a2f4bf02ddef129cf61a9aaee9240f9d6b2
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-16 21:35:17 +0900

    [Silver I] Title: 괄호의 값, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2504. 괄호의 값/괄호의 값.py
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.py
@@ -4,41 +4,35 @@ stack = []
 
 temp = 1
 result = 0
-
 for i in range(len(arr)):
     if arr[i] == '(':
-        stack.append(arr[i])
         temp *= 2
-
-    elif arr[i] == '[':
         stack.append(arr[i])
+    elif arr[i] == '[':
         temp *= 3
-
+        stack.append(arr[i])
     elif arr[i] == ')':
-        if not stack or stack[-1] != '(':
+        if not stack or stack[-1] == '[':
             result = 0
             break
 
         if arr[i-1] == '(':
             result += temp
         stack.pop()
-        temp = temp // 2
-
+        temp //= 2
     else:
-        if not stack or stack[-1] != '[':
+        if not stack or stack[-1] == '(':
             result = 0
             break
 
         if arr[i-1] == '[':
             result += temp
-
         stack.pop()
-        temp = temp // 3
+        temp //= 3
 
 if stack:
     print(0)
 else:
     print(result)
-
 
 
