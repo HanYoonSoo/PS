4f8c282d831e130b8bf5a43dfce0d13bb20df619
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-17 13:19:43 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
@@ -1,18 +1,19 @@
 import sys
-
 input = sys.stdin.readline
-
 N, M, x, y, K = map(int, input().split())
 
 graph = []
 dice = [0, 0, 0, 0, 0, 0]
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0] # 동, 서, 북, 남
-
 for _ in range(N):
     graph.append(list(map(int, input().split())))
 
-command = list(map(int, input().split()))
+turn = list(map(int, input().split()))
+
+dice[1] = graph[x][y]
+graph[x][y] = 0
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
 def move(dir):
     global dice
@@ -24,28 +25,26 @@ def move(dir):
         dice = list((a, e, c, f, d, b))
     elif dir == 2:
         dice = list((d, a, b, c, e, f))
-    else:
+    elif dir == 3:
         dice = list((b, c, d, a, e, f))
 
-for i in command:
-    x = x + dx[i-1]
-    y = y + dy[i-1]
+for command in turn:
+    x = x + dx[command-1]
+    y = y + dy[command-1]
 
-    if x < 0 or y < 0 or x >= N or y >= M:
-        x -= dx[i-1]
-        y -= dy[i-1]
-        continue
-
-    move(i-1)
-
-    if graph[x][y] == 0:
-        graph[x][y] = dice[1]
+    if 0 <= x < N and 0 <= y < M:
+        move(command - 1)
+        if graph[x][y] == 0:
+            graph[x][y] = dice[1]
+        else:
+            dice[1] = graph[x][y]
+            graph[x][y] = 0
+        print(dice[3])
     else:
-        dice[1] = graph[x][y]
-        graph[x][y] = 0
+        x -= dx[command-1]
+        y -= dy[command-1]
 
-    print(dice[3])
-
+    
 
 
 
