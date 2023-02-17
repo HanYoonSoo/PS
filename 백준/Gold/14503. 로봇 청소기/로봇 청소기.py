2936c4311a5bb773dea7198aba10a3d0c6be7dd1
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-17 12:19:02 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,9 +1,10 @@
 import sys
 
 input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
-nx, ny, dir = map(int, input().split())
+x, y, dir = map(int, input().split())
 
 graph = []
 
@@ -11,46 +12,34 @@ for _ in range(N):
     graph.append(list(map(int, input().split())))
 
 dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1] # 북 동 남 서
+dy = [0, 1, 0, -1]
 
-dir_idx = 0
-
-count = 0
-
-if graph[nx][ny] == 0:
-    graph[nx][ny] = 2
-    count = 1
-
+graph[x][y] = 2
+count = 1
 compare = 0
 
 while True:
-
     dir = (dir + 3) % 4
-    nx = nx + dx[dir]
-    ny = ny + dy[dir]
 
-    if 1 <= nx < N and 1 <= ny < M and graph[nx][ny] == 0:
-        graph[nx][ny] = 2
+    x += dx[dir]
+    y += dy[dir]
+
+    if 0 <= x < N and 0 <= y < M and graph[x][y] == 0:
+        graph[x][y] = 2
         count += 1
         compare = 0
-
     else:
-        nx -= dx[dir]
-        ny -= dy[dir]
+        x -= dx[dir]
+        y -= dy[dir]
         compare += 1
 
     if compare == 4:
-        nx -= dx[dir]
-        ny -= dy[dir]
+        x -= dx[dir]
+        y -= dy[dir]
 
-        if 1 <= nx < N and 1 <= ny < M:
-            if graph[nx][ny] == 1:
-                break
-            else:
-                compare = 0
-        else:
+        if 0 <= x < N and 0 <= y < M and graph[x][y] == 1:
             break
+        else:
+            compare = 0
 
 print(count)
-
-
