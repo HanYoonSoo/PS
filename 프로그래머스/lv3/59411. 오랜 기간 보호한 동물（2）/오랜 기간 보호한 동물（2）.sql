bb1793a0447d545b2cd7aa32ec9b9453c6386451
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-19 22:14:52 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,3 +1,3 @@
 select ao.animal_id, ao.name
 from animal_outs ao inner join animal_ins ai on ao.animal_id = ai.animal_id
-order by datediff(ao.datetime, ai.datetime) desc limit 2;
+order by (ao.datetime - ai.datetime) desc limit 2;
