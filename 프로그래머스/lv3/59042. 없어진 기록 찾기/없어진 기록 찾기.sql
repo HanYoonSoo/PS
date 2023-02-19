445cca608d62de4979d0692ef3151abd939ab066
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-19 22:22:20 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT ao.animal_id, ao.name
+select ao.animal_id, ao.name
 from animal_outs ao left join animal_ins ai on ao.animal_id = ai.animal_id
 where ai.animal_id is null
-order by ao.animal_id;
+order by ao.animal_id
