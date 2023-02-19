872eafd702fb8b4d43534ad73c9116185ad80b23
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-19 22:10:16 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT id, name, host_id
+select id, name, host_id
 from places
-where host_id in (select host_id from places group by host_id having count(host_id) >= 2)
+where host_id in (select host_id from places group by host_id having count(*) >= 2)
 order by id;
