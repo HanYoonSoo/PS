f65cabc1c91682d609775caeb21d6351b17bb56e
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-20 14:12:43 +0900

    [Silver II] Title: 에디터, Time: 312 ms, Memory: 37576 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+stack1 = list(input().rstrip())
+stack2 = []
+
+N = int(input().rstrip())
+
+for _ in range(N):
+    command = list(input().split())
+
+    if command[0] == 'L':
+        if stack1:
+            stack2.append(stack1.pop())
+    elif command[0] == 'D':
+        if stack2:
+            stack1.append(stack2.pop())
+    elif command[0] == 'B':
+        if stack1:
+            stack1.pop()
+    else:
+        stack1.append(command[1])
+
+stack1.extend(reversed(stack2))
+
+print(''.join(stack1))
+
