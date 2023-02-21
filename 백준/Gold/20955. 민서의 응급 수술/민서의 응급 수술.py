97fa4eb60f862fe830084a03d6e835b71a891e76
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-21 13:40:56 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 236 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.py
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.py
@@ -6,8 +6,6 @@ N, M = map(int, input().split())
 
 parent = list(range(N+1))
 
-result = 0
-
 def find_parent(parent, p):
     if parent[p] == p:
         return p
@@ -17,26 +15,29 @@ def find_parent(parent, p):
     return parent[p]
 
 def union(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+    a = find_parent(parent, parent[a])
+    b = find_parent(parent, parent[b])
 
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
 
+count = 0
+
 for _ in range(M):
     a, b = map(int, input().split())
+
     if find_parent(parent, a) == find_parent(parent, b):
-        result += 1
+        count += 1
     union(parent, a, b)
 
 idx = 1
 
 for i in range(2, N+1):
     if find_parent(parent, i) != find_parent(parent, idx):
-        result += 1
+        count += 1
         union(parent, i, idx)
         idx = i
 
-print(result)
+print(count)
