81b30192328091bfed91754856a2a3cea5c3d3e8
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-22 17:26:50 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 244 ms, Memory: 51428 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,34 +1,55 @@
 import sys
 import heapq
 
+INF = 1e9
+
 input = sys.stdin.readline
 
-V, E = map(int, input().split())
+def prim():
+    V, E = map(int, input().split())
 
-edges = []
-for _ in range(E):
-    edges.append(list(map(int, input().split())))
+    graph = [[] for _ in range(V+1)]
 
-edges.sort(key=lambda x : x[2])
+    for _ in range(E):
+        a, b, w = map(int, input().split())
 
-parent = [i for i in range(0, V+1)]
+        graph[a].append((w, b))
+        graph[b].append((w, a))
 
-def get_parent(v):
-    if parent[v] == v:
-        return v
+    visited = [False] * (V+1)
 
-    parent[v] = get_parent(parent[v])
-    return parent[v]
+    heap = [(0, 1)]
 
-def same_parent(a, b):
-    if get_parent(a) == get_parent(b):
-        return True
-    else:
-        return False
+    result = 0
+    count = 0
 
-def union_parent(a, b):
-    a = get_parent(a)
-    b = get_parent(b)
+    while heap:
+        if count == V:
+            print(result)
+            return
+
+        w, node = heapq.heappop(heap)
+
+        if not visited[node]:
+            result += w
+            count += 1
+            visited[node] = True
+
+            for v_w, v in graph[node]:
+                heapq.heappush(heap, (v_w, v))
+
+#########################################
+def find_parent(parent, p):
+    if parent[p] == p:
+        return p
+
+    parent[p] = find_parent(parent, parent[p])
+
+    return parent[p]
+
+def union(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
 
     if a < b:
         parent[b] = a
@@ -36,13 +57,25 @@ def union_parent(a, b):
         parent[a] = b
 
 def kruskal():
+    V, E = map(int, input().split())
+
+    parent = [i for i in range(V+1)]
+    edges = []
+
+    for _ in range(E):
+        a, b, c = map(int, input().split())
+        edges.append((a, b, c))
+
+    edges.sort(key = lambda x : x[2])
+
     result = 0
-    for a, b, dist in edges:
-        if not same_parent(a, b):
-            union_parent(a, b)
-            result += dist
 
-    return result
+    for a, b, c in edges:
+        if find_parent(parent, a) != find_parent(parent, b):
+            union(parent, a, b)
+            result += c
 
+    print(result)
 
-print(kruskal())
+kruskal()
+#prim()
