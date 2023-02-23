7dad96c47f46add6acf2f9c644f132c9a8c00f9c
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-23 21:06:13 +0900

    [Gold III] Title: 아기 상어, Time: 312 ms, Memory: 34340 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,72 +1,60 @@
+import sys
 from collections import deque
-import copy
 
-class Node:
-    def __init__(self, x, y, dist):
-        self.x = x
-        self.y = y
-        self.dist = dist
+input = sys.stdin.readline
 
-N = int(input())
+N = int(input().rstrip())
 
-graph = []
-q = deque()
+grid = []
 
+x, y = 0, 0
 for i in range(N):
-    graph.append(list(map(int, input().split())))
-    if 9 in graph[i]:
-        q.append(Node(i, graph[i].index(9), 0))
-        graph[i][graph[i].index(9)] = 0
+    temp = list(map(int, input().split()))
+    for j in range(N):
+        if temp[j] == 9:
+            x, y = i, j
+    grid.append(temp)
 
-size = 2
-time = 0
-eat = 0
+grid[x][y] = 0
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+size = 2
+eat = 0
+time = 0
+q = deque()
+q.append((x, y, 0))
+
 while True:
     fish = []
-    dist = [[0] * N for _ in range(N)]
+    visited = [[False] * N for _ in range(N)]
+
     while q:
-        node = q.popleft()
+        x, y, count = q.popleft()
 
         for i in range(4):
-            nx = node.x + dx[i]
-            ny = node.y + dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < N and dist[nx][ny] == 0 and graph[nx][ny] <= size:
-                dist[nx][ny] = node.dist + 1
-                q.append(Node(nx, ny, dist[nx][ny]))
-
-                if 1 <= graph[nx][ny] <= 6 and graph[nx][ny] < size:
-                    fish.append(Node(nx, ny, dist[nx][ny]))
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and grid[nx][ny] <= size:
+                visited[nx][ny] = True
+                q.append((nx, ny, count + 1))
+                if 1 <= grid[nx][ny] <= 6 and grid[nx][ny] < size:
+                    fish.append((nx, ny, count + 1))
 
     if len(fish) == 0:
         print(time)
-        exit(0)
+        break
 
-    currentFish = fish[0]
+    fish.sort(key=lambda x: (x[2], x[0], x[1]))
+    a, b, c = fish[0]
 
-    for i in range(1, len(fish)):
-        if currentFish.dist > fish[i].dist:
-            currentFish = fish[i]
-        elif currentFish.dist == fish[i].dist:
-            if currentFish.x > fish[i].x:
-                currentFish = fish[i]
-            elif currentFish.x == fish[i].x:
-                if currentFish.y > fish[i].y:
-                    currentFish = fish[i]
-
-    time += currentFish.dist
+    time += c
+    grid[a][b] = 0
+    q.append((a, b, 0))
     eat += 1
 
-    if eat == size:
-        size += 1
+    if eat >= size:
         eat = 0
-
-    graph[currentFish.x][currentFish.y] = 0
-    q.append(Node(currentFish.x, currentFish.y, 0))
-
-
-
+        size += 1
