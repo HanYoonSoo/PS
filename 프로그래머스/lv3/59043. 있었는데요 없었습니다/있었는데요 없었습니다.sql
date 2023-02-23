f644601cc540cc86218091d6ba6c0f9f3cfd8241
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-23 16:11:37 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,4 +1,4 @@
 select ao.animal_id, ao.name
 from animal_outs ao inner join animal_ins ai on ao.animal_id = ai.animal_id
-where ao.datetime < ai.datetime
-order by ai.datetime
+where ai.datetime > ao.datetime
+order by ai.datetime;
