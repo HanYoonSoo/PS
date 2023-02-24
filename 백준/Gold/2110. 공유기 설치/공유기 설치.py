5daefa67d844549657ddb7a2d08f8f4ffdad1502
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-24 17:16:55 +0900

    [Gold IV] Title: 공유기 설치, Time: 528 ms, Memory: 38984 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -16,19 +16,21 @@ end = arr[-1] - arr[0]
 
 result = 0
 while start <= end:
+    count = 1
     mid = (start + end) // 2
     value = arr[0]
-    count = 1
 
-    for i in range(1, N):
-        if arr[i] >= value + mid:
-            count += 1
+    for i in range(1, len(arr)):
+        if value + mid <= arr[i]:
             value = arr[i]
+            count += 1
 
     if count >= C:
         start = mid + 1
-        result = mid
+        result = max(result, mid)
     else:
         end = mid - 1
 
 print(result)
+
+
