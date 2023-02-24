bb2c2930525ae1fe90a0d3a759d2a26e4d005627
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-24 15:56:18 +0900

    [Gold V] Title: 경쟁적 전염, Time: 1588 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -7,40 +7,41 @@ N, K = map(int, input().split())
 
 virus = [[] for _ in range(K+1)]
 
-graph = []
+grid = []
 
 for i in range(N):
     temp = list(map(int, input().split()))
     for j in range(N):
         if temp[j] != 0:
             virus[temp[j]].append((i, j))
-    graph.append(temp)
+    grid.append(temp)
 
-#print(virus[1])
 S, X, Y = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
 def bfs(x, y, num, temp_virus):
+    q = deque()
+    q.append((x, y))
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == 0:
-            graph[nx][ny] = num
-            temp_virus.append((nx, ny))
+        if 0 <= nx < N and 0 <= ny < N and grid[nx][ny] == 0:
+            grid[nx][ny] = num
+            if (nx, ny) not in temp_virus:
+                temp_virus.append((nx, ny))
 
-
-for i in range(S):
-    for j in range(1, K + 1):
+for _ in range(S):
+    for i in range(1, K+1):
         temp_virus = []
-        for x, y in virus[j]:
-            bfs(x, y, j, temp_virus)
-        virus[j] = temp_virus
+        for x, y in virus[i]:
+            bfs(x, y, i, temp_virus)
+        virus[i] = temp_virus
 
-if graph[X-1][Y-1] == 0:
-    print(0)
+if grid[X-1][Y-1] != 0:
+    print(grid[X-1][Y-1])
 else:
-    print(graph[X-1][Y-1])
+    print(0)
