691092fff2dca41c5f233634d87075d6a4a86628
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-24 11:16:56 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.93 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -14,12 +14,13 @@ def rotate_key_90_degree(key):
 def check(new_lock):
     n = len(new_lock) // 3
     
-    for i in range(n, 2 * n):
-        for j in range(n, 2 * n):
+    for i in range(n, n * 2):
+        for j in range(n, n * 2):
             if new_lock[i][j] != 1:
                 return False
     
     return True
+
 def solution(key, lock):
     n = len(lock)
     m = len(key)
@@ -30,7 +31,7 @@ def solution(key, lock):
         for j in range(n):
             new_lock[i + n][j + n] = lock[i][j]
     
-    for rotation in range(4):
+    for i in range(4):
         key = rotate_key_90_degree(key)
         for x in range(n * 2):
             for y in range(n * 2):
@@ -44,4 +45,8 @@ def solution(key, lock):
                 for i in range(m):
                     for j in range(m):
                         new_lock[x+i][y+j] -= key[i][j]
+
     return False
+                
+                
+                        
