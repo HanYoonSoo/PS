fcf0b1ece9d2bb36944fcd099cdf6766b930a346
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-24 18:32:27 +0900

    [Silver II] Title: 병사 배치하기, Time: 756 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -1,18 +1,19 @@
 import sys
 
-inpit = sys.stdin.readline
+input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-number = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-number.reverse()
+arr.reverse()
 
 dp = [1] * N
-
+# 4 2 5 8 4 11 15
 for i in range(1, N):
-    for j in range(0, i):
-        if number[i] > number[j]:
+    for j in range(i):
+        if arr[i] > arr[j]:
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(N - max(dp))
+
