59b3fad8a7f12307191443b01133b0017c20b6e0
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-02-24 12:12:33 +0900

    [level 3] Title: 기둥과 보 설치, Time: 1444.66 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/lv3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,28 +1,29 @@
 def possible(result):
-    for frame in result:
-        x, y, struct = frame
-        
-        if struct == 0:
-            if y == 0 or [x-1, y, 1] in result or [x, y, 1] in result or [x, y - 1, 0] in result:
+    
+    for x, y, a in result:
+        if a == 0:
+            if y == 0 or (x - 1, y, 1) in result or (x, y, 1) in result or (x, y - 1, 0) in result:
                 continue
             return False
-        if struct == 1:
-            if [x, y-1, 0] in result or [x+1, y-1, 0] in result or ([x-1, y, 1] in result and [x+1, y, 1] in result):
+        elif a == 1:
+            if (x, y - 1, 0) in result or (x + 1, y - 1, 0) in result or ((x-1, y, 1) in result and (x + 1, y, 1) in result):
                 continue
             return False
+    
     return True
+
 def solution(n, build_frame):
     result = []
     
-    for frame in build_frame:
-        x, y, struct, operate = frame
-        if operate == 1:
-            result.append([x, y, struct])
+    for x, y, a, b in build_frame:
+        if b == 1:
+            result.append((x, y, a))
             if not possible(result):
-                result.remove([x, y, struct])
-        if operate == 0:
-            result.remove([x, y, struct])
+                result.remove((x, y, a))
+        elif b == 0:
+            result.remove((x, y, a))
             if not possible(result):
-                result.append([x, y, struct])
+                result.append((x, y, a))
     
     return sorted(result)
+            
