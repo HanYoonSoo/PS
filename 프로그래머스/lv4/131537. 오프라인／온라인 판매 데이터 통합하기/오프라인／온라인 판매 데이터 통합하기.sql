675ae84c38c797426b3e3e92008c2deaef36c6fd
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-03-02 20:17:18 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,11 +1,11 @@
--- 코드를 입력하세요
-SELECT date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, user_id, sales_amount
+select date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, user_id, sales_amount
 from online_sale
-where sales_date between '2022-03-01' and '2022-03-31'
+where sales_date like '2022-03%'
 
 union all
 
-select date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, null as user_id, sales_amount
+select sales_date, product_id, null as user_id, sales_amount
 from offline_sale
-where sales_date between '2022-03-01' and '2022-03-31'
+where sales_date like '2022-03%'
+
 order by sales_date, product_id, user_id;
