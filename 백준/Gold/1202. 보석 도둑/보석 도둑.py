1c099216c208b4c8f7c4e99833ddbcf42d2bfee4
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-03-03 17:51:05 +0900

    [Gold II] Title: 보석 도둑, Time: 1532 ms, Memory: 86688 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,29 +1,31 @@
 import sys
 import heapq
 
-N, K = map(int, sys.stdin.readline().split())
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
 
 jew = []
 bag = []
 
 for _ in range(N):
-    jew.append(list(map(int, sys.stdin.readline().split())))
+    a, b = map(int, input().split())
+    jew.append((a, b))
 
 for _ in range(K):
-    bag.append(int(sys.stdin.readline()))
+    bag.append(int(input().rstrip()))
 
 jew.sort()
 bag.sort()
 
-temp = []
-answer = 0
+heap = []
 
+result = 0
 for b in bag:
-    # print(b)
     while jew and b >= jew[0][0]:
-        heapq.heappush(temp, -heapq.heappop(jew)[1])
-     #   print(jew)
-    if temp:
-        answer -= heapq.heappop(temp)
+        heapq.heappush(heap, -heapq.heappop(jew)[1])
 
-print(answer)
+    if heap:
+        result -= heapq.heappop(heap)
+
+print(result)
