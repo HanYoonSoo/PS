c572402476053eb93b9a34d91922088769040661
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-05-06 14:49:45 +0900

    [Gold V] Title: 합분해, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,14 +1,18 @@
+def count_sum_combinations(N, K):
+    dp = [[0] * (N + 1) for _ in range(K + 1)]  # 동적 계획법을 위한 2차원 배열 생성
+
+    for i in range(K + 1):
+        dp[i][0] = 1  # 합이 0인 경우는 1가지 방법으로 가능
+
+    for i in range(1, K + 1):
+        for j in range(1, N + 1):
+            dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 1000000000  # 1,000,000,000으로 나눈 나머지 계산
+
+    return dp[K][N]
+
+# 입력 받기
 N, K = map(int, input().split())
 
-dp = [[0] * (201) for _ in range(201)]
-
-for i in range(201):
-    dp[1][i] = 1
-
-for i in range(2, 201):
-    dp[i][1] = i
-    for j in range(2, 201):
-        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000
-
-
-print(dp[K][N])
+# 결과 출력
+result = count_sum_combinations(N, K)
+print(result)
