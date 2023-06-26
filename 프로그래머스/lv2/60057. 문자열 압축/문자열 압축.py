7cc5daa8cd579bb588248e1b03be43e297f8a909
HanYoonSoo <114587653+HanYoonSoo@users.noreply.github.com>
2023-06-26 14:25:51 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,23 +1,22 @@
 def solution(s):
     result = len(s)
     
-    for i in range(1, len(s)//2 + 1):
+    for i in range(1, len(s) // 2 + 1):
         idx = 0
         tmp = ''
         while idx < len(s):
-            c = s[idx:idx+i]
+            c = s[idx: idx + i]
             n = 1
-            while c == s[idx+i:idx+i+i]:
+            while c == s[idx + i: idx + i + i]:
                 n += 1
-                idx = idx + i
+                idx += i
             if n != 1:
                 tmp += str(n) + c
             else:
                 tmp += c
             
-            idx = idx + i
+            idx += i
         result = min(result, len(tmp))
     
     return result
-            
         
