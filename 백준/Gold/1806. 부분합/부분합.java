1a7b9460a614b27c3d3bbca33acfeeb74ce10d83
HanYoonSoo <hdbstn3055@gmail.com>
2024-04-03 00:01:41 +0900

    [Gold IV] Title: 부분합, Time: 300 ms, Memory: 24128 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,40 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
-
+/**
+ * 10000 이하의 자연수로 이루어진 길이 N짜리 수열
+ * 이 수열에서 "연속된 수들의 부분합 중에 그 합이 S 이상이 되는 것 중, 가장 짧은 것의 길이"
+ */
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int N = scan.nextInt();
-		int S = scan.nextInt();
-		
-		int[] arr = new int[N+1];
-		
-		for(int i = 0; i < N; i++) {
-			arr[i] = scan.nextInt();
-		}
-		
-		int total = 0;
-		int end = 0;
-		int start = 0;
-		int min = Integer.MAX_VALUE;
-		
-		while(end <= N && start <= N) {
-			if(total >= S && min > end- start)
-				min = end - start;
-			else if(total < S) {
-				total += arr[end++];
-			}
-			else {
-				total -= arr[start++];
-			}
-		}
-		
-		if(min == Integer.MAX_VALUE) {
-			System.out.println(0);
-		}
-		else {
-			System.out.println(min);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+        int right = 0;
+
+        int end = N;
+
+        int min_len = Integer.MAX_VALUE;
+        int compare = 0;
+        while (right <= end && left <= end) {
+            if(compare >= S && min_len > right - left)
+                min_len = right - left;
+            else if(compare < S)
+                compare += num[right++];
+            else
+                compare -= num[left++];
+        }
+
+        if(min_len == Integer.MAX_VALUE){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(min_len);
+        }
+    }
 }
