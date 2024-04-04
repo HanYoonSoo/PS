bb26c534bef17faaa5718b9c8363b86322c8468e
HanYoonSoo <hdbstn3055@gmail.com>
2024-04-04 21:23:27 +0900

    [Gold V] Title: 감소하는 수, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,34 +1,49 @@
-	
-import java.util.*;
- 
-public class Main {    
-    
-    static ArrayList<Long> list;
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int n = scan.nextInt();
-        list = new ArrayList<>();
-        
-        if(n <= 10) System.out.println(n);
-        else if(n > 1022) System.out.println("-1");
-        else {
-            for(int i = 0; i < 10; i++) {
-                bp(i, 1);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1, 2, 3, 4, 5, 6, 7, 8, 9            9
+ * 10, 20, 21, 30, 31, 32, 40, 41, 42, 43, 50, 51, 52, 53, 54    45
+ * 210, 320, 321, 310, 432, 431, 430, 421, 420, 410, 510, 520, 521, 530, 531, 532, 540, 541, 542, 543
+ */
+
+// 2 - 1, 3 - 3, 4 - 6, 5 - 10, 6 - 15, 7 - 21, 8 - 28, 9 - 3
+// 9 8 7 6 5 4 3 2 1
+public class Main {
+    static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N <= 10)
+            System.out.println(N);
+        else if(N > 1022){
+            System.out.println(-1);
+        }
+        else{
+            for(int i = 0; i < 10; i++){
+                find_num(i, 1);
             }
             Collections.sort(list);
- 
-            System.out.println(list.get(n));
+            System.out.println(list.get(N));
         }
-    }    
-    
-    public static void bp(long num, int idx) {
-        if(idx > 10) return;
-        
+    }
+
+    private static void find_num(long num, int index) {
+        if(index > 10)
+            return;
+
         list.add(num);
-        for(int i = 0; i < num % 10; i++) {
-            bp((num * 10) + i, idx + 1);
+
+        // 9 - 1, 98 - 2, 987 - 3, 9876 - 4, 98765 - 5, 987654 - 6, 9876543 - 7, 98765432 - 8, 987654321 - 9, 9876543210 - 10
+        for(int i = 0; i < num % 10; i++){
+            find_num((num * 10) + i, index + 1);
         }
     }
 }
