ba31efe5d297967745f985729a13018771915dda
HanYoonSoo <hdbstn3055@gmail.com>
2024-07-24 22:23:50 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 144 ms, Memory: 19028 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,49 +1,54 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    static class Point {
-        int x;
-        int count;
-        Point(int x, int count){
-            this.x=x;
-            this.count=count;
-        }
-    }
-    static int n,m;
-    static int answer =Integer.MAX_VALUE;
-    static boolean []board;
-    static void bfs(int start, int end){
-        Queue<Point>q=new LinkedList<>();
-        q.add(new Point(start,0));
-        board[start]=true;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+
+        int N = Integer.parseInt(arr[0]);
+        int K = Integer.parseInt(arr[1]);
+
+        Queue<int[]> q = new LinkedList<>();
+
+        q.add(new int[]{N, 0});
+
+        boolean[] visited = new boolean[100001];
+        visited[N] = true;
+
+        int result = Integer.MAX_VALUE;
+
         while(!q.isEmpty()){
-            Point frog=q.poll();
-            int x = frog.x;
-            int count = frog.count;
-            if(x==end){
-                answer = Math.min(answer,count);
-            }
-            if(2*x<=100000 && !board[2*x]){
-                board[2*x]=true;
-                q.add(new Point(2*x,count));
-            }
-            if(x-1>=0 && !board[x-1]){
-                board[x-1]=true;
-                q.add(new Point(x-1,count+1));
-            }
-            if(x+1<=100000 && !board[x+1]){
-                board[x+1]=true;
-                q.add(new Point(x+1,count+1));
+            int[] curr = q.poll();
+
+            int point = curr[0];
+            int time = curr[1];
+
+            if(point == K){
+                result = Math.min(result, time);
             }
 
+            if(point * 2 <= 100000 && !visited[point * 2]){
+                q.add(new int[]{point * 2, time});
+                visited[point * 2] = true;
+            }
+
+            if(point - 1 >= 0 && !visited[point - 1]){
+                q.add(new int[]{point - 1, time + 1});
+                visited[point - 1] = true;
+            }
+            
+            if(point + 1 <= 100000 && !visited[point + 1]){
+                q.add(new int[]{point + 1, time + 1});
+                visited[point + 1] = true;
+            }
+            
         }
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        board = new boolean [100001];
-        bfs(n,m);
-        System.out.println(answer);
+
+        System.out.println(result);
     }
 }
