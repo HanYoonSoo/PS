658ba3c596a2b13fe0012bdf157d719af322e64f
HanYoonSoo <hdbstn3055@gmail.com>
2024-08-03 16:17:43 +0900

    [Gold IV] Title: 최단경로, Time: 864 ms, Memory: 119864 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,88 +1,84 @@
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	static List<List<Node>> graph;
-	static int V;
-	static int E;
-	static int[] distance;
-	static int start;
-	static int INF = Integer.MAX_VALUE;
-	static boolean[] check;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		V = sc.nextInt();
-		E = sc.nextInt();
-		start = sc.nextInt();
-		
-		graph = new ArrayList<>();
-		distance = new int[V + 1];
-		check = new boolean[V + 1];
-		for (int i = 0; i <= V; i++) {
-			graph.add(new ArrayList<>());
-		}
-		
-		Arrays.fill(distance, INF);
-		distance[start] = 0;
-		
-		for (int i = 0; i < E; i++) {
-			int v1 = sc.nextInt();
-			int v2 = sc.nextInt();
-			int w = sc.nextInt();
-			graph.get(v1).add(new Node(v2, w));
-		}
-		
-		dijkstra();
-		for (int i = 1; i <= V; i++) {
-			if (distance[i] == INF) {
-				System.out.println("INF");
-			} else {
-				System.out.println(distance[i]);
-			}
-		}
-	}
 
-	private static void dijkstra() {
-		PriorityQueue<Node> queue = new PriorityQueue<>();
-		queue.add(new Node(start, 0));
-		
-		while (!queue.isEmpty()) {
-			Node node = queue.poll();
-			if (check[node.v]) {
-				continue;
-			}
-			
-			check[node.v] = true;
-			
-			for(Node n : graph.get(node.v)) {
-				int totalW = n.w + node.w;
-				if(distance[n.v] > totalW) {
-					distance[n.v] = totalW;
-					queue.add(new Node(n.v, totalW));
-				}
-			}
-			
-		}
+    static class Edge implements Comparable<Edge>{
+        int end;
+        int weight;
 
-	}
+        public Edge(int end, int weight){
+            this.end = end;
+            this.weight = weight;
+        }
 
-	public static class Node implements Comparable<Node> {
-		int v;
-		int w;
+        @Override
+        public int compareTo(Edge e){
+            return this.weight - e.weight;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		public Node(int v, int w) {
-			this.v = v;
-			this.w = w;
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		@Override
-		public int compareTo(Node o) {
-			return w - o.w;
-		}
-	}
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        int K = Integer.parseInt(br.readLine());
+
+        List<List<Edge>> grid = new ArrayList<>();
+
+        for(int i = 0; i <= V; i++){
+            grid.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < E; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            grid.get(a).add(new Edge(b, c));
+        }
+
+        int[] dist = new int[V + 1];
+        boolean[] visited = new boolean[V + 1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+        pq.add(new Edge(K, 0));
+        dist[K] = 0;
+
+        while(!pq.isEmpty()){
+            Edge curr = pq.poll();
+
+            if(visited[curr.end])
+                continue;
+
+            visited[curr.end] = true;
+
+            for(Edge edge : grid.get(curr.end)){
+                if(dist[edge.end] > dist[curr.end] + edge.weight){
+                    dist[edge.end] = dist[curr.end] + edge.weight;
+                }
+
+                pq.add(new Edge(edge.end, dist[edge.end]));
+            }
+        }
+
+        for(int i = 1; i <= V; i++){
+            if(dist[i] != Integer.MAX_VALUE){
+                System.out.println(dist[i]);
+            } else{
+                System.out.println("INF");
+            }
+        }
+
+    }
 }
