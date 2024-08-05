e7ee4c03759a8dbab2744037e81483b55d06d8f0
HanYoonSoo <hdbstn3055@gmail.com>
2024-08-05 20:11:36 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16628 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,74 +1,59 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] grid;
-	static boolean[][] visited;
-	static int[][] distance;
-	static int dx[] = {-1, 1, 0, 0};
-	static int dy[] = {0, 0, -1, 1};
- 	static int N;
-	static int M;
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		N = scan.nextInt();
-		M = scan.nextInt();
-		scan.nextLine();
-		
-		grid = new int[N][M];
-		visited = new boolean[N][M];
-		distance = new int[N][M];
-		
-		for(int i = 0; i < N; i++) {
-			String[] temp = scan.nextLine().split("");
-			for(int j = 0;  j < temp.length; j++) {
-				grid[i][j] = Integer.parseInt(temp[j]);
-			}
-		}
-		
-		System.out.println(bfs(0, 0));
-	}
-	
-	public static int bfs(int x, int y) {
-		visited[x][y] = true;
-		distance[x][y] = 1;
-		Queue<MPoint> queue = new LinkedList<>();
-		
-		queue.add(new MPoint(x, y));
-		
-		while(!queue.isEmpty()) {
-			MPoint temp = queue.poll();
-			
-			if(temp.x == N-1 && temp.y == M-1) {
-				return distance[N-1][M-1];
-			}
-			
-			for(int i = 0; i < 4; i++) {
-				int nx = temp.x + dx[i];
-				int ny = temp.y + dy[i];
-				
-				if(nx >= 0 && nx < N && ny >= 0 && ny < M && visited[nx][ny] == false && grid[nx][ny] == 1) {
-					distance[nx][ny] = distance[temp.x][temp.y] + 1;
-					queue.add(new MPoint(nx, ny));
-					visited[nx][ny] = true;
-				}
-			}
-		}
-		
-		return 0;
-	}
-}
+    static int N;
+    static int M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] dy = {-1, 1, 0, 0};
+        int[] dx = {0, 0, -1, 1};
 
-class MPoint{
-	int x;
-	int y;
-	
-	public MPoint(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int[][] grid = new int[N][M];
+        boolean[][] visited = new boolean[N][M];
+
+        for(int i = 0; i < N; i++){
+            String[] arr = br.readLine().split("");
+            for(int j = 0; j < M; j++){
+                grid[i][j] = Integer.parseInt(arr[j]);
+            }
+        }
+
+        visited[0][0] = true;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        q.add(new int[]{0, 0, 1});
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+
+            int y = curr[0];
+            int x = curr[1];
+            int count = curr[2];
+
+            if(y == (N - 1) && x == (M - 1)){
+                System.out.println(count);
+                break;
+            }
+
+            for(int i = 0; i < 4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if(0 <= ny && ny < N && 0 <= nx && nx < M && !visited[ny][nx] && grid[ny][nx] == 1){
+                    q.add(new int[]{ny, nx, count + 1});
+                    visited[ny][nx] = true;
+                }
+            }
+        }
+    }
 }
