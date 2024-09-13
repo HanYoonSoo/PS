b25b2d0f774c3fd219f310f5404a7b5dc1abf9c4
HanYoonSoo <hdbstn3055@gmail.com>
2024-09-13 20:21:55 +0900

    [Gold IV] Title: 전구와 스위치, Time: 192 ms, Memory: 18360 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
@@ -3,59 +3,84 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static int[] dx = {-1, 0, 1};
+    public static int n;
+    public static char[] data1;
+    public static char[] data2;
+    public static char[] data3;
 
-    static int N;
-    static boolean[] targetArr;
-    static int result = Integer.MAX_VALUE;
-
-    public static void main(String[] rags) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        String input1 = br.readLine();
+        String input2 = br.readLine();
+        data1 = new char[n];
+        data2 = new char[n];
+        data3 = new char[n];
 
-        N = Integer.parseInt(br.readLine());
-
-        String start = br.readLine();
-        String target = br.readLine();
-
-        boolean[] arrA = new boolean[N];
-        boolean[] arrB = new boolean[N];
-        targetArr = new boolean[N];
-
-        for(int i = 0; i < N; i++){
-            arrA[i] = start.charAt(i) == '0';
-            arrB[i] = start.charAt(i) == '0';
-            targetArr[i] = target.charAt(i) == '0';
+        for(int i=0;i<n;i++){
+            data1[i] = input1.charAt(i);
+            data2[i] = data1[i];
+            data3[i] = input2.charAt(i);
         }
 
-        dfs(1, 0, arrA);
-        dfs(1, 1, turnOn(0, arrB));
+        int ans = Integer.MAX_VALUE;
 
-        System.out.println(result == Integer.MAX_VALUE ? -1 : result);
+        int cnt1 = 0;
+        int cnt2 = 0;
 
-    }
+        data1 = change(data1, 0);
+        cnt1++;
 
-    public static void dfs(int idx, int count, boolean[] arr){
-        if(idx == N){
-            if(arr[idx - 1] == targetArr[idx-1]){
-                result = Math.min(count, result);
-            }
-            return;
-        }
-
-        if(arr[idx - 1] != targetArr[idx - 1]){
-            dfs(idx + 1, count + 1, turnOn(idx, arr));
-        }
-        else{
-            dfs(idx + 1, count, arr);
-        }
-    }
-
-    public static boolean[] turnOn(int idx, boolean[] arr){
-        for(int i = idx - 1; i <= idx + 1; i++){
-            if(i >=0 && i < N){
-                arr[i] = !arr[i];
+        for(int i=1;i<n;i++){
+            if(data1[i-1] != data3[i-1]){
+                data1 = change(data1, i);
+                cnt1++;
             }
         }
 
-        return arr;
+        if(data1[n-1] == data3[n-1]){
+            ans = Math.min(ans, cnt1);
+        }
+
+        for(int i=1;i<n;i++){
+            if(data2[i-1] != data3[i-1]){
+                data2 = change(data2, i);
+                cnt2++;
+            }
+        }
+
+        if(data2[n-1] == data3[n-1]){
+            ans = Math.min(ans, cnt2);
+        }
+
+
+        if(ans == Integer.MAX_VALUE){
+            System.out.println(-1);
+        } else{
+            System.out.println(ans);
+        }
+
+
+
+
+
+
+    }
+
+    public static char[] change(char[] data, int i){
+        for (int d = 0; d < 3; d++) {
+            int nx = i + dx[d];
+            if (nx < 0 || nx > n - 1) {
+                continue;
+            }
+
+            if (data[nx] == '0') {
+                data[nx] = '1';
+            } else {
+                data[nx] = '0';
+            }
+        }
+        return data;
     }
 }
