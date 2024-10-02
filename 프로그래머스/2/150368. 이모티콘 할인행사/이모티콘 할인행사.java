15a19c89cd03e36654654d874be923d16e2aaf47
HanYoonSoo <hdbstn3055@gmail.com>
2024-10-02 15:06:13 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 78.03 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,55 +1,56 @@
 import java.util.*;
 
 class Solution {
-    int[] sales = {10, 20, 30, 40};
-    PriorityQueue<int[]> pq;
+    int[] sales = new int[]{10, 20, 30, 40};
     int[] rate;
+    
+    PriorityQueue<int[]> pq;
     
     public int[] solution(int[][] users, int[] emoticons) {
         pq = new PriorityQueue<>((o1, o2) -> {
-            if(o1[0] == o2[0])
+            if(o1[0] == o2[0]){
                 return o2[1] - o1[1];
-            else
-                return o2[0] - o1[0];
+            }
+            
+            return o2[0] - o1[0];
         });
+        
         rate = new int[emoticons.length];
         
-        perm(0, users, emoticons);
+        dfs(0, users, emoticons);
         
         return pq.poll();
     }
     
-    public void perm(int idx, int[][] users, int[] emoticons){
-        if(idx == rate.length){
+    public void dfs(int idx, int[][] users, int[] emoticons){
+        if(idx == emoticons.length){
             int total = 0;
-            int service = 0;
+            int count = 0;
             
             for(int i = 0; i < users.length; i++){
                 int sum = 0;
-                
-                for(int j = 0; j < rate.length; j++){
+                for(int j = 0; j < emoticons.length; j++){
                     if(users[i][0] <= rate[j]){
-                        sum += emoticons[j] * (100 - rate[j]) / 100;
-                    } 
+                        sum += (emoticons[j] * (100 - rate[j]) / 100);
+                    }
                 }
                 
                 if(sum >= users[i][1]){
-                    service++;
+                    count++;
                     sum = 0;
                 }
                 
-                // System.out.println(sum);
-                
                 total += sum;
             }
-            pq.add(new int[]{service, total});
+            
+            pq.add(new int[]{count, total});
             
             return;
         }
         
-        for(int i = 0; i < sales.length; i++){
+        for(int i = 0; i < 4; i++){
             rate[idx] = sales[i];
-            perm(idx + 1, users, emoticons);
+            dfs(idx + 1, users, emoticons);
         }
     }
 }
