c114174776ddc416641bfa8b55c5759800f0cfa1
HanYoonSoo <hdbstn3055@gmail.com>
2024-10-14 14:23:44 +0900

    [Gold IV] Title: 오큰수, Time: 956 ms, Memory: 169672 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,40 +1,44 @@
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+    //4
+    //3 5 2 7
     public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	int size = Integer.parseInt(br.readLine());
-    	String[] str = br.readLine().split(" ");
-    	StringBuilder sb = new StringBuilder();
-    	Stack<Integer> stack = new Stack<Integer>();
-    	Stack<Integer> result = new Stack<Integer>();
-    	Stack<Integer> temp = new Stack<Integer>();
-    	
-    	for(int i=0; i<size; i++) {
-    		stack.add(Integer.parseInt(str[i]));
-    	}
-    	
-    	while(!stack.isEmpty()) {
-    		if(temp.isEmpty()) {
-    			result.add(-1);
-    			temp.add(stack.pop());
-    		}else {
-    			if(stack.peek() < temp.peek()) {
-    				result.add(temp.peek());
-    				temp.add(stack.pop());
-    			}else {
-    				temp.pop();
-    			}
-    		}
-    	}
-    	
-    	while(!result.isEmpty()) {
-    		sb.append(result.pop()).append(" ");
-    	}
-    	
-    	System.out.print(sb.toString());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] result = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = N - 1; i >= 0; i--){
+            while(!stack.isEmpty() && stack.peek() <= arr[i]){
+                stack.pop();
+            }
+
+            if(stack.isEmpty()){
+                result[i] = -1;
+            } else
+                result[i] = stack.peek();
+            stack.add(arr[i]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int num : result){
+            sb.append(num + " ");
+        }
+
+        System.out.println(sb);
     }
 }
-
