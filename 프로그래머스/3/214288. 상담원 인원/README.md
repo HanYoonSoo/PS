d60dd8acafddb86b349278f18cb4899fa5b6d30f
HanYoonSoo <hdbstn3055@gmail.com>
2024-10-14 14:46:13 +0900

    [level 3] Title: 상담원 인원, Time: 224.57 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/3/214288. 상담원 인원/README.md
+++ 프로그래머스/3/214288. 상담원 인원/README.md
@@ -1,10 +1,10 @@
 # [level 3] 상담원 인원 - 214288 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/214288?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/214288) 
 
 ### 성능 요약
 
-메모리: 107 MB, 시간: 122.26 ms
+메모리: 97.2 MB, 시간: 224.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 12일 19:47:05
+2024년 10월 14일 14:46:10
 
 ### 문제 설명
 
--- 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
@@ -1,87 +1,73 @@
 import java.util.*;
 
 class Solution {
-    int[] size;
     int K;
     int N;
     int[][] arr;
     int result = Integer.MAX_VALUE;
-    Set<String> set = new HashSet<>();
-    
+    int[] sizes;
     public int solution(int k, int n, int[][] reqs) {
         K = k;
         N = n;
         arr = reqs;
         
-        size = new int[k + 1];
         
-        Arrays.fill(size, 1);
+        sizes = new int[K + 1];
         
-        dfs(K, 1);
+        Arrays.fill(sizes, 1);
+        
+        dfs(1, K);
         
         return result;
     }
     
-    public void dfs(int sum, int idx){
-//         StringBuilder sb = new StringBuilder();
-        
-//         for(int i = 1; i <= K; i++){
-//             sb.append(size[i]);
-//         }
-        
-//         if(!set.contains(sb.toString())){
-//             set.add(sb.toString());
-//         } else{
-//             return;
-//         }
-        
-        if(sum == N){
+    public void dfs(int idx, int count){
+        if(count == N){
             Map<Integer, PriorityQueue<int[]>> map = new HashMap<>();
-            
-            int compute = 0;
             
             for(int i = 1; i <= K; i++){
                 map.put(i, new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]));
             }
             
+            int total = 0;
+            
+            // for(int i = 1; i <= K; i++){
+            //     System.out.print(sizes[i] + " ");
+            // }
+            
+            // System.out.println();
+            
             for(int i = 0; i < arr.length; i++){
-                int start = arr[i][0];
-                int end = arr[i][1];
-                int team = arr[i][2];
+                int startTime = arr[i][0];
+                int useTime = arr[i][1];
+                int human = arr[i][2];
                 
-                if(map.get(team).size() >= size[team]){
-                    if(map.get(team).peek()[1] <= start){
-                        map.get(team).poll();
-                        map.get(team).add(new int[]{start, start + end});
+                if(map.get(human).size() < sizes[human]){
+                    map.get(human).add(new int[]{startTime, startTime + useTime});
+                } else{
+                    int[] curr = map.get(human).poll();
+                    
+                    // System.out.println(curr[1]);
+                    if(curr[1] <= startTime){
+                        map.get(human).add(new int[]{startTime, startTime + useTime});
                     } else{
-                        compute += (map.get(team).peek()[1] - start);
-                        int num = map.get(team).poll()[1];
-                        map.get(team).add(new int[]{start, num + end});
+                        total += curr[1] - startTime;
+                        map.get(human).add(new int[]{curr[1], curr[1] + useTime});
                     }
-                } else if(map.get(team).size() < size[team]){
-                    map.get(team).add(new int[]{start, start + end});
-                }
-                
-                if(compute >= result){
-                    return;
                 }
             }
             
-//             for(int i = 1; i <= K; i++){
-//                 System.out.print(size[i] + " ");
-//             }
-//             System.out.println();
+            // System.out.println(total);
             
-//             System.out.println(compute);
-            result = Math.min(result, compute);
+            result = Math.min(result, total);
             
             return;
         }
         
         for(int i = idx; i <= K; i++){
-            size[i] += 1;
-            dfs(sum + 1, i);
-            size[i] -= 1;
+            sizes[i]++;
+            dfs(i, count + 1);
+            sizes[i]--;
         }
     }
 }
