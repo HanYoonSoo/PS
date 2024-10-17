b08b57a1c8746fb35e47eb1db7dc65f0d906c45a
HanYoonSoo <hdbstn3055@gmail.com>
2024-10-17 17:53:11 +0900

    [Gold III] Title: 과제, Time: 116 ms, Memory: 14880 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -1,64 +1,54 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static List<SNode> subject;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		subject = new ArrayList<>();
-		
-		String[] temp;
-		int maxDay = 0;
-		for(int i = 0; i < N; i++) {
-			temp = br.readLine().split(" ");
-			subject.add(new SNode(Integer.parseInt(temp[0]),Integer.parseInt(temp[1])));
-			
-			maxDay = Math.max(maxDay, Integer.parseInt(temp[0]));
-		}
-		
-		int result = 0;
-		
-		for(int i = maxDay; i > 0; i--) {
-			result += searchMaxValue(i);
-		}
-		
-		System.out.println(result);
-	}
-	
-	public static int searchMaxValue(int now) {
-		int index = -1;
-		int result = 0;
-		
-		for(int i = 0; i < subject.size(); i++) {
-			if(subject.get(i).day >= now && result < subject.get(i).value) {
-				index = i;
-				result = subject.get(i).value;
-			}
-		}
-		
-		if(result == 0) {
-			return 0;
-		}
-		
-		subject.remove(index);
-		return result;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            if(o1[0] == o2[0])
+                return o2[1] - o1[1];
+            return o2[0] - o1[0];
+        });
+
+        int maxDay = 0;
+
+        StringTokenizer st;
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int d = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            maxDay = Math.max(d, maxDay);
+
+            pq.add(new int[]{d, w});
+        }
+
+        int total = 0;
+        
+        PriorityQueue<Integer> result = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        for(int i = maxDay; i >= 1; i--){
+            while(!pq.isEmpty() && pq.peek()[0] >= i){
+                result.add(pq.poll()[1]);
+            }
+
+            if(!result.isEmpty()){
+                total += result.poll();
+            }
+        }
+
+        System.out.println(total);
+    }
 }
 
-class SNode{
-	int day;
-	int value;
-	
-	public SNode(int day, int value) {
-		this.day = day;
-		this.value = value;
-	}
-	
-}
+// 6 5
+//
+
+// 1 2 4 5 7
