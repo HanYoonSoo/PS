3a36e3b10e1621b33fa33b99542d5667233c161a
HanYoonSoo <hdbstn3055@gmail.com>
2024-10-18 17:31:08 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 67.41 ms, Memory: 214 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,49 +1,64 @@
+import java.util.*;
 class Solution {
-    int N, M;
-    int[][] sum;
-    public int solution(int[][] board, int[][] skills) {
-        N = board.length;
-        M = board[0].length;
+    
+    int[][] res;
+    int[][] board;
+    int n, m;
+    
+   	void update(int r1, int c1, int r2, int c2, int amount) {
+        r1++; c1++; r2++; c2++;
         
-        sum = new int[N + 1][M + 1];
+    	board[r1][c1] -= amount;
+        board[r1][c2 + 1] += amount;
+       	board[r2 + 1][c1] += amount;
+        board[r2 + 1][c2 + 1] -= amount;
+    } 
+    
+    void psum() {
+        for(int i = 1; i <= n; i++) 
+           	for(int j = 2; j <= m; j++) 
+                board[i][j] += board[i][j - 1];
         
-        for(int[] skill : skills){
-            int type = skill[0];
-            int y1 = skill[1];
-            int x1 = skill[2];
-            int y2 = skill[3];
-            int x2 = skill[4];
-            int power = skill[5];
-            
-            power = power * (type == 1 ? -1 : 1);
-            
-            sum[y1][x1] += power;
-            sum[y1][x2 + 1] -= power;
-            sum[y2 + 1][x1] -= power;
-            sum[y2 + 1][x2 + 1] += power;
-        }
+		for(int j = 1; j <= m; j++)
+        	for(int i = 2; i <= n; i++) 
+                board[i][j] += board[i - 1][j];
         
-        for(int i = 0; i <= N; i++){
-            for(int j = 1; j <= M; j++){
-                sum[i][j] += sum[i][j-1];
+        for(int i = 1; i <= n; i++) 
+           	for(int j = 1; j <= m; j++) 
+                res[i][j] += board[i][j];
+    }
+    
+    int calcCnt() {
+        int ret = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(res[i][j] > 0) ret++;
             }
         }
-        
-        for(int j = 0; j <= M; j++){
-            for(int i = 1; i <= N; i++){
-                sum[i][j] += sum[i-1][j];
-            }
-        }
-        
+        return ret;
+    }
+    
+    public int solution(int[][] board, int[][] skill) {
         int answer = 0;
+        n = board.length;
+        m = board[0].length;
         
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if((board[i][j] + sum[i][j]) > 0){
-                    answer++;
-                }
+        this.board = new int[n + 2][m + 2];
+        res = new int[n + 2][m + 2];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                res[i + 1][j + 1] = board[i][j];
+        
+       	
+        for(int[] arr : skill) {
+            if(arr[0] == 1) {
+               	update(arr[1], arr[2], arr[3], arr[4], arr[5]);
+            } else {
+               	update(arr[1], arr[2], arr[3], arr[4], -arr[5]);
             }
         }
+        psum();
+       	answer = calcCnt(); 
         
         return answer;
     }
