cce4ada78c23a8d2d97d76544e1a473f0776cd25
HanYoonSoo <hdbstn3055@gmail.com>
2024-10-19 22:15:05 +0900

    [Silver I] Title: 회전 초밥, Time: 216 ms, Memory: 21380 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -26,29 +24,50 @@ public class Main {
 
         int[] count = new int[d + 1];
 
+        Map<Integer, Integer> map = new HashMap<>();
+
         int max = 0;
+//        for(int i = 0; i < k; i++){
+//            if(count[arr[i]] == 0)
+//                max++;
+//            count[arr[i]]++;
+//        }
+
         for(int i = 0; i < k; i++){
-            if(count[arr[i]] == 0)
-                max++;
-            count[arr[i]]++;
+            map.put(arr[i], map.getOrDefault(arr[i], 0 ) + 1);
         }
 
-        if(count[c] == 0){
+        max = map.size();
+
+        if(!map.containsKey(c))
             max++;
-        }
 
         for(int i = k; i < N + k; i++){
-            count[arr[(i - k) % N]]--;
-            count[arr[i % N]]++;
+//            count[arr[(i - k) % N]]--;
+//            count[arr[i % N]]++;
+            int check = map.get(arr[(i - k) % N]);
 
-            int temp = 0;
-            for(int j = 1; j <= d; j++){
-                if(count[j] > 0){
-                    temp++;
-                }
+            if(check == 1){
+                map.remove(arr[(i - k) % N]);
+            } else{
+                map.put(arr[(i - k) % N], check - 1);
             }
 
-            if(count[c] == 0){
+            map.put(arr[i % N], map.getOrDefault(arr[i % N], 0) + 1);
+
+            int temp = map.size();
+
+//            for(int j = 1; j <= d; j++){
+//                if(count[j] > 0){
+//                    temp++;
+//                }
+//            }
+
+//            if(count[c] == 0){
+//                temp++;
+//            }
+
+            if(!map.containsKey(c)){
                 temp++;
             }
 
