d6f261afe150a247eaaea677b996756e6865ecac
HanYoonSoo <hdbstn3055@gmail.com>
2024-10-22 13:45:47 +0900

    [Gold II] Title: 마피아, Time: 468 ms, Memory: 19660 KB -BaekjoonHub

--- 백준/Gold/1079. 마피아/마피아.java
+++ 백준/Gold/1079. 마피아/마피아.java
@@ -10,6 +10,7 @@ public class Main {
     static int mafia;
     static int result = 0;
     static boolean[] isDied;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -20,51 +21,43 @@ public class Main {
         isDied = new boolean[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             guilty[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            for(int j = 0; j < N; j++){
+            for (int j = 0; j < N; j++) {
                 grid[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         mafia = Integer.parseInt(br.readLine());
 
-        if(N % 2 == 0){
+        if (N % 2 == 0) {
             dfs(false, N, 0);
-        } else{
+        } else {
             dfs(true, N, 0);
         }
 
         System.out.println(result);
     }
 
-    public static void dfs(boolean day, int alive, int count){
-//        for(int i = 0; i < N; i++){
-//            System.out.print(guilty[i] + " ");
-//            System.out.print(isDied[i] + " ");
-//        }
-//        System.out.println();
-
-//        System.out.println(day + " " + alive + " " + count);
+    public static void dfs(boolean day, int alive, int count) {
         result = Math.max(count, result);
-        if(alive == 1){
+        if (alive == 1) {
 //            result = Math.max(count, result);
             return;
         }
-//        result = Math.max(count, result);
 
-        if(day){
+        if (day) {
             int max = 0;
             int maxIdx = -1;
-            for(int i = 0; i < N; i++){
-                if(!isDied[i]){
-                    if(max <= guilty[i]){
-                        if(max < guilty[i]){
+            for (int i = 0; i < N; i++) {
+                if (!isDied[i]) {
+                    if (max <= guilty[i]) {
+                        if (max < guilty[i]) {
                             maxIdx = i;
                         }
 
@@ -73,27 +66,22 @@ public class Main {
                 }
             }
 
-            if(maxIdx == mafia){
-//                System.out.println(count);
-//                System.exit(0);
-                return;
-            } else{
+            if(maxIdx != mafia) {
                 isDied[maxIdx] = true;
-//                System.out.println(maxIdx + " " + (count) + " " + alive);
                 dfs(false, alive - 1, count);
                 isDied[maxIdx] = false;
             }
-        } else{
-            for(int i = 0; i < N; i++){
-                if(!isDied[i] && i != mafia){
-                    for(int j = 0; j < N; j++){
+
+        } else {
+            for (int i = 0; i < N; i++) {
+                if (!isDied[i] && i != mafia) {
+                    for (int j = 0; j < N; j++) {
                         guilty[j] += grid[i][j];
                     }
                     isDied[i] = true;
-//                    System.out.println(i + " " + (count + 1) + " " + alive);
                     dfs(true, alive - 1, count + 1);
                     isDied[i] = false;
-                    for(int j = 0; j < N; j++){
+                    for (int j = 0; j < N; j++) {
                         guilty[j] -= grid[i][j];
                     }
 
