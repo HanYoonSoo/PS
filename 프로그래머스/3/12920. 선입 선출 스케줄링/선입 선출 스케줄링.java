5845142aaa5d84c86f9a804a38123dcbf1fb8cdf
HanYoonSoo <hdbstn3055@gmail.com>
2024-10-26 21:52:42 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 7.22 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -1,56 +1,47 @@
 import java.util.*;
 
 class Solution {
-     public static int solution(int n, int[] cores) {
-        int answer = 0;
-
-        int min = 0; // 시간의 최소값
-        int max = 10000*n; // 시간의 최대값
-
-        int time = 0;
-        int m = 0; // time까지 처리한 작업량
-
-        while (min <= max) {  
-            int mid = (min+max)/2;
-
-            int count = CountWork(mid, cores);
-            // System.out.println(count);
+     public static long solution(int n, int[] cores) {
+         long left = 1L;
+         long right = 500000001L;
+         
+         while(left <= right){
+            long mid = (left + right) / 2;
+             
+            long sum = cores.length;
+            for(int i = 0; i < cores.length; i++){
+                sum += (mid / cores[i]);
+            } 
             
-            if (count >= n) { // 해당시간까지 처리한 작업량보다 n이 크면 time과 m갱신
-                max = mid-1;   
-                time = mid;     
-                m = count; 
-            }else{
-                min = mid+1;
-            }
-        }
-
-        m-=n; // 처리한 작업량과 n의 차이 갱신
-        for(int i = cores.length-1; i>=0; i--){
-            if (time % cores[i] == 0) { // 시간이 time일때, 작업을 처리하는 core
-                if (m == 0) {
-                    answer = i+1;
-                    break;
-                }
-                m--;
-            }
-        }
-
-        return answer;
-    }
-
-    static int CountWork(int time, int[] cores){
-
-        if (time == 0) { // 시간이 0일 때, 처리할 수 있는 작업량은 코어의 개수 
-            return cores.length;
-        }
-
-        int count = cores.length; // 시간이 0일 때, 처리한 작업량 
-
-        for(int i = 0; i<cores.length; i++){ // time까지 코어가 처리할 수 있는 작업량 합산
-            count += (time/cores[i]);
-        }
-
-        return count;
+             if(sum < n){
+                 left = mid + 1;
+             } else{
+                 right = mid - 1;
+             }
+         }
+         
+         System.out.println(left);
+         
+         long total = cores.length;
+         
+         for(int i = 0; i < cores.length; i++){
+             total += ((left - 1) / cores[i]);
+             // System.out.println(total);
+         }
+         
+         for(int i = 0; i < cores.length; i++){
+             if(left % cores[i] == 0){
+                 total++;
+                 if(total == n){
+                     return i + 1;
+                 }
+             }
+         }
+         
+         return -1;
     }
 }
+
+// 0 - 1 2 3 - 3
+// 1 - 1 - 4
+// 2 - 1 2 - 6
