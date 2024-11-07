bdc7f3a3f898432e08b47774ee46735c908b42c3
HanYoonSoo <hdbstn3055@gmail.com>
2024-11-07 19:40:20 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 7.41 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/README.md
+++ 프로그래머스/3/150365. 미로 탈출 명령어/README.md
@@ -1,10 +1,10 @@
 # [level 3] 미로 탈출 명령어 - 150365 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 0.05 ms
+메모리: 80.8 MB, 시간: 7.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 22:32:32
+2024년 11월 07일 19:40:17
 
 ### 문제 설명
 
--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -3,113 +3,73 @@ import java.util.Queue;
 
 /**
  * n x m (x, y) -k-> (r, c)
+ * dlru
  */
 class Solution {
-    static int n;
-    static int m;
-    static int x;
-    static int y;
-    static int r;
-    static int c;
-    static int k;
-    static String grid[][];
-    static int visited[][];
-
+    int[] dx = {1, 0, 0, -1};
+    int[] dy = {0, -1, 1, 0};
+    
+    int[][] grid;
+    boolean[][] visited;
+    
+    String[] str = {"d", "l", "r", "u"};
+    
     public String solution(int n1, int m1, int x1, int y1, int r1, int c1, int k1) {
-        n = n1;
-        m = m1;
-        x = x1;
-        y = y1;
-        r = r1;
-        c = c1;
-        k = k1;
-
-        int distance = Math.abs(x - r) + Math.abs(y - c);
-
-        // 거리가 안되면 "impossible" 출력
-        // (1, 1) -> (6, 1) 인데 k가 4인 경우
-        if(distance > k){
+        grid = new int[n1 + 1][m1 + 1];
+        visited = new boolean[n1 + 1][m1 + 1];
+        
+        int distance = Math.abs(x1 - r1) + Math.abs(y1 - c1);
+        
+        if(distance > k1)
             return "impossible";
-        }
-
-        // 거리는 되지만 도착지에 도달할 수 없는 경우
-        // (1, 1) -> (2, 1)로 갈 때 k가 4인 경우 3의 거리를 가야되는데 나머지 2 했을 때 0이 나오지 않으면 갈 수 없음
-        if((distance - k) % 2 != 0){
+        
+        if((k1 - distance) % 2 == 1)
             return "impossible";
-        }
-
-        grid = new String[n+1][m+1];
-        visited = new int[n+1][m+1];
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(i == x && j == y){
-                    grid[i][j] = "S";
-                }
-                else if(i == r && j == c){
-                    grid[i][j] = "E";
-                }
-                else{
-                    grid[i][j] = ".";
-                }
-            }
-        }
-
-        String result = bfs(x, y);
-        return result;
-    }
-
-    private static String bfs(int a, int b) {
-        // 알파벳의 우선 순위가 높은 순(사전 순)으로 탐색 진행
-        int[] dx = {1, 0, 0, -1};
-        int[] dy = {0, -1, 1, 0};
-        String[] move = {"d", "l", "r", "u"};
-
+        
+        // System.out.println("1");
+        
         Queue<Node> q = new LinkedList<>();
-
-        q.add(new Node(a, b, ""));
-
-        // bfs 반복문 시작
+        
+        q.add(new Node(x1, y1, 0, ""));
+        visited[x1][y1] = true;
+        
         while(!q.isEmpty()){
             Node curr = q.poll();
-
-            int x = curr.x;
-            int y = curr.y;
-            String cur_str = curr.result;
-
-            // k만큼 이동하고 도착지라면 반환
-            if(visited[x][y] == k && x == r && y == c){
-                return curr.result;
+        
+            
+            if(curr.x == r1 && curr.y == c1 && curr.count == k1){
+                return curr.s;
             }
-            else if(visited[x][y] > k){
-                break;
-            }
-
+            
             for(int i = 0; i < 4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(1 <= nx && nx <= n && 1 <= ny && ny <= m){
-                    if(Math.abs(nx - r) + Math.abs(ny - c) + visited[x][y] + 1 > k)
+                int nx = curr.x + dx[i];
+                int ny = curr.y + dy[i];
+                
+                if(1 <= nx && nx <= n1 && 1 <= ny && ny <= m1){
+                    // visited[nx][ny] = true;
+                    if(Math.abs(nx - r1) + Math.abs(ny - c1) + curr.count > k1)
                         continue;
-                    visited[nx][ny] = visited[x][y] + 1;
-                    q.add(new Node(nx, ny, cur_str.concat(move[i])));
+                    
+                    q.add(new Node(nx, ny, curr.count + 1, curr.s.concat(str[i])));
                     break;
                 }
             }
         }
-        return "";
+        
+        return "impossible";
     }
-
-    static class Node{
+    
+    public class Node{
         int x;
         int y;
-        String result;
-
-        public Node(int x, int y, String result) {
+        int count;
+        String s;
+        
+        public Node(int x, int y, int count, String s){
             this.x = x;
             this.y = y;
-            this.result = result;
+            this.count = count;
+            this.s = s;
         }
     }
 }
