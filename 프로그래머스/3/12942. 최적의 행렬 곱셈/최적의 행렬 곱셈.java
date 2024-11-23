303ecc14baceb1967c847460c20bea95b00c2458
HanYoonSoo <hdbstn3055@gmail.com>
2024-11-23 11:25:28 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 25.29 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.java
+++ 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.java
@@ -1,26 +1,6 @@
 class Solution {
     public int solution(int[][] matrix_sizes) {
-        int N = matrix_sizes.length;
-        
-        int[][] dp = new int[201][201];
-        
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N - i; j++){
-                int a = j;
-                int b = j + i;
-                
-                if(a == b)
-                    dp[a][b] = 0;
-                else{
-                    dp[a][b] = Integer.MAX_VALUE;
-                    
-                    for(int k = a; k < b; k++){
-                        dp[a][b] = Math.min(dp[a][b], dp[a][k] + dp[k + 1][b] + matrix_sizes[a][0] * matrix_sizes[k][1] * matrix_sizes[b][1]);
-                    }
-                }
-            }
-        }
-        
-        return dp[0][N - 1];
+        int answer = 0;
+        return answer;
     }
 }
