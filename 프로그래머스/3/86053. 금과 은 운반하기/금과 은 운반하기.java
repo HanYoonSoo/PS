16fc8918b5ca0d71831949c6ea56a08fadf401bf
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-08 18:54:34 +0900

    [level 3] Title: 금과 은 운반하기, Time: 126.21 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -1,43 +1,5 @@
 class Solution {
-    int N;
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        N = g.length;
-        
-        long left = 1L;
-        long right = 400000000000000L;
-        
-        while(left <= right){
-            long mid = (left + right) / 2;
-            
-            if(isPossible(a, b, g, s, w, t, mid)){
-                right = mid - 1;
-            } else{
-                left = mid + 1;
-            }
-        }
-        
-        return left;
-    }
-    
-    public boolean isPossible(int a, int b, int[] g, int[] s, int[] w, int[] t, long time){
-        long total = 0;
-        long totalG = 0;
-        long totalS = 0;
-        
-        for(int i = 0; i < N; i++){
-            long count = time / (2L * t[i]);
-            
-            if(time % (2L * t[i]) >= t[i])
-                count++;
-            
-            long temp = Math.min(w[i] * count, g[i] + s[i]);
-            
-            total += temp;
-            totalG += Math.min(temp, g[i]);
-            totalS += Math.min(temp, s[i]);
-        }
-        
-        return total >= (a + b) && totalG >= a && totalS >= b;
         
     }
 }
