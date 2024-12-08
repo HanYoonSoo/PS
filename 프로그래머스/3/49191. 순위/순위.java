539a4a925c32bbef05faf29957aa0da2b9a07422
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-08 19:44:27 +0900

    [level 3] Title: 순위, Time: 16.76 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,5 +1,20 @@
 import java.util.*;
 
+/**
+* 모든 정점을 다 접근해야 함
+* 1 > 2, 2 > 3 ==> 1 > 3
+* 1 < 2, 2 < 3 ==> 1 < 3
+*
+* 공통된 값은 고정으로 두고 나머지 노드 비교해야 함
+* 
+* i k
+* k j
+*
+* 1 1, 1 2, 1 3, 1 4, 1 5 에서 k가 5라고 가정하면
+* 5 1, 5 2, 5 3, 5 4, 5 5  위 아래 비교
+*
+* graph[i][k] == graph[k][j] --> i > j 
+*/
 class Solution {
     public int solution(int n, int[][] results) {
         int[][] graph = new int[n + 1][n + 1];
@@ -12,11 +27,9 @@ class Solution {
             graph[b][a] = -1;
         }
         
-        int answer = 0;
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                for(int k = 1; k <= n; k++){
+        for(int k = 1; k <= n; k++){
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= n; j++){
                     if(graph[i][k] == 1 && graph[k][j] == 1){
                         graph[i][j] = 1;
                         graph[j][i] = -1;
@@ -28,20 +41,23 @@ class Solution {
             }
         }
         
+        int result = 0;
+        
         for(int i = 1; i <= n; i++){
+            
             int count = 0;
+            
             for(int j = 1; j <= n; j++){
-                if(graph[i][j] != 0){
+                if(i != j && graph[i][j] != 0){
                     count++;
-                }
+                }    
             }
             
             if(count == n - 1){
-                answer++;
+                result++;
             }
-
         }
         
-        return answer;
+        return result;
     }
 }
