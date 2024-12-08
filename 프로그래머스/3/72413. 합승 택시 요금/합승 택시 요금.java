0c11ea0fa9971841cce05508990c431b711aff83
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-08 20:25:17 +0900

    [level 3] Title: 합승 택시 요금, Time: 30.27 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -2,42 +2,87 @@ import java.util.*;
 
 class Solution {
     
-    int INF = 200 * 100000 + 1;
+    class Edge implements Comparable<Edge>{
+        int end;
+        int w;
+        
+        public Edge(int end, int w){
+            this.end = end;
+            this.w = w;
+        }
+        
+        @Override
+        public int compareTo(Edge e){
+            return this.w - e.w;
+        }
+    }
+    
+    List<Edge>[] graph;
+    int INF = 200 * 100000 + 1;;
     
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int[][] graph = new int[n + 1][n + 1];
+        graph = new ArrayList[n + 1];
         
         for(int i = 1; i <= n; i++){
-            Arrays.fill(graph[i], INF);
-            graph[i][i] = 0;
+            graph[i] = new ArrayList<>();
         }
         
-        int m = fares.length;
-        
-        for(int i = 0; i < m; i++){
-            int v1 = fares[i][0];
-            int v2 = fares[i][1];
-            int c = fares[i][2];
+        for(int[] fare : fares){
+            int v1 = fare[0];
+            int v2 = fare[1];
+            int w = fare[2];
             
-            graph[v1][v2] = c;
-            graph[v2][v1] = c;
+            graph[v1].add(new Edge(v2, w));
+            graph[v2].add(new Edge(v1, w));
         }
         
-        for(int k = 1; k <= n; k++){
-            for(int i = 1; i <= n; i++){
-                for(int j = 1; j <= n; j++){
-                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+        int[] dist = new int[n + 1];
+        int[] distA = new int[n + 1];
+        int[] distB = new int[n + 1];
+        
+        Arrays.fill(dist, INF);
+        Arrays.fill(distA, INF);
+        Arrays.fill(distB, INF);
+        
+        dist = dijkstra(s, dist);
+        distA = dijkstra(a, distA);
+        distB = dijkstra(b, distB);
+        
+        int minA = distA[s];
+        int minB = distB[s];
+        
+        int result = minA + minB;
+        
+        for(int i = 1; i <= n; i++){
+            result = Math.min(dist[i] + distA[i] + distB[i], result);
+        }
+        
+        return result;
+    }
+    
+    public int[] dijkstra(int node, int[] dist){
+        dist[node] = 0;
+        
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        
+        pq.add(new Edge(node, 0));
+        
+        while(!pq.isEmpty()){
+            Edge curr = pq.poll();
+            
+            if(dist[curr.end] < curr.w)
+                continue;
+            
+            for(Edge edge : graph[curr.end]){
+                if(dist[edge.end] > dist[curr.end] + edge.w){
+                    dist[edge.end] = dist[curr.end] + edge.w;
+                    
+                    pq.add(new Edge(edge.end, dist[edge.end]));
                 }
             }
         }
         
-        int answer = graph[s][a] + graph[s][b];
-        
-        for(int i = 1; i <= n; i++){
-            answer = Math.min(answer, graph[s][i] + graph[i][a] + graph[i][b]);
-        }
-        
-        
-        return answer;
+        return dist;
     }
+    
 }
