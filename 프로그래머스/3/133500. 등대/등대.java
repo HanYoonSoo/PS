fa24f5e30a599a5b7a84baf8f1a257ef964bf114
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-09 14:39:57 +0900

    [level 3] Title: 등대, Time: 59.77 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -1,48 +1,50 @@
 import java.util.*;
 
 class Solution {
+    
+    List<Integer>[] tree;
     int N;
-    List<List<Integer>> tree;
     int result = 0;
     
     public int solution(int n, int[][] lighthouse) {
         N = n;
+        tree = new ArrayList[N + 1];
         
-        tree = new ArrayList<>();
-        
-        for(int i = 0; i <= N; i++){
-            tree.add(new ArrayList<>());
+        for(int i = 1; i <= N; i++){
+            tree[i] = new ArrayList<>();
         }
         
         for(int[] light : lighthouse){
-            tree.get(light[0]).add(light[1]);
-            tree.get(light[1]).add(light[0]);
+            int a = light[0];
+            int b = light[1];
+            
+            tree[a].add(b);
+            tree[b].add(a);
         }
         
-        dfs(1, 0);
+        dfs(1, -1);
         
         return result;
     }
     
-    public int dfs(int root, int before){
-        if(tree.get(root).size() == 1 && tree.get(root).get(0) == before){
+    public int dfs(int root, int parent){
+        if(tree[root].size() == 1 && tree[root].get(0) == parent){
             return 1;
         }
         
-        int compare = 0;
-        
-        for(int node : tree.get(root)){
-            if(node != before){
-                compare += dfs(node, root);
+        int compute = 0;
+        for(int node : tree[root]){
+            if(node != parent){
+                compute += dfs(node, root);
             }
         }
         
-        if(compare == 0){
-            // System.out.println(root);
+        if(compute == 0){
             return 1;
         }
         
         result++;
         return 0;
     }
+    
 }
