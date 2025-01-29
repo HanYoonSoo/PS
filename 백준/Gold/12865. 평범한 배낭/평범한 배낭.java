52539912bfd2e3a2c05b16bff6dbf6e9d71249fd
HanYoonSoo <hdbstn3055@gmail.com>
2025-01-29 20:11:36 +0900

    [Gold V] Title: 평범한 배낭, Time: 180 ms, Memory: 53812 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,34 +1,39 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int N = scan.nextInt();
-		int K = scan.nextInt();
-		
-		int W[] = new int[N+1];
-		int V[] = new int[N+1];
-		
-		for(int i = 1; i <= N; i++) {
-			W[i] = scan.nextInt();
-			V[i] = scan.nextInt();
-		}
-		
-		int[][] dp = new int[N+1][K+1];
-		
-		for(int i = 1; i <= N; i++) {
-			for(int j = 1; j <= K; j++) {
-				if(W[i] > j) {
-					dp[i][j] = dp[i-1][j];
-				}
-				else {
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
-				}
-			}
-		}
-		
-		System.out.println(dp[N][K]);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] w = new int[N + 1];
+        int[] v = new int[N + 1];
+
+        for(int i = 1; i <= N; i++){
+            st = new StringTokenizer(br.readLine());
+
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[N + 1][K + 1];
+        int max = 0;
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= K; j++){
+                if(j >= w[i]) {
+                    dp[i][j] = Math.max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
 }
