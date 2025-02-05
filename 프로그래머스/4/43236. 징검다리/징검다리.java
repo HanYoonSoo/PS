ddc40351b8ba45e605ceb0b98f083e2ee1366dad
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-05 20:51:31 +0900

    [level 4] Title: 징검다리, Time: 43.00 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        Arrays.sort(rocks);
+        
+        int left = 1;
+        int right = distance;
+        
+        int result = 0;
+        
+        while(left <= right){
+            int mid = (left + right) / 2;
+            int remove = 0;
+            int prev = 0;
+            
+            for(int rock : rocks){
+                if(rock - prev < mid){
+                    remove++;
+                } else
+                    prev = rock;
+            }
+            
+            if(distance - prev < mid){
+                remove++;
+            }
+            
+            if(remove <= n){
+                result = mid;
+                left = mid + 1;            
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return result;
+    }
+}
