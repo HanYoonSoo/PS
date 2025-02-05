261a718e1782f9a5e00cb6f3422c57e18af6e8ce
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-05 20:15:36 +0900

    [level 3] Title: N으로 표현, Time: 18.23 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,9 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        if(N == number)
-            return 1;
-        
         List<Set<Integer>> dp = new ArrayList<>();
         
         for(int i = 0; i <= 8; i++){
@@ -14,26 +11,23 @@ class Solution {
         dp.get(1).add(N);
         
         for(int i = 2; i <= 8; i++){
-            StringBuilder sb = new StringBuilder();
+            String num = String.valueOf(N).repeat(i);
             
-            for(int j = 0; j < i; j++){
-                sb.append(N);
-            }
-            
-            dp.get(i).add(Integer.parseInt(sb.toString()));
+            dp.get(i).add(Integer.valueOf(num));
             
             for(int j = 1; j < i; j++){
                 int k = i - j;
-                
                 for(int num1 : dp.get(j)){
                     for(int num2 : dp.get(k)){
-                        if(num1 + num2 > 0)
-                            dp.get(i).add(num1 + num2);
-                        
-                        if(num1 - num2 > 0)
-                            dp.get(i).add(num1 - num2);
-                        
                         dp.get(i).add(num1 * num2);
+                        
+                        if(num1 + num2 > 0){
+                            dp.get(i).add(num1 + num2);
+                        }
+                        
+                        if(num1 - num2 > 0){
+                            dp.get(i).add(num1 - num2);
+                        }
                         
                         if(num2 != 0){
                             dp.get(i).add(num1 / num2);
