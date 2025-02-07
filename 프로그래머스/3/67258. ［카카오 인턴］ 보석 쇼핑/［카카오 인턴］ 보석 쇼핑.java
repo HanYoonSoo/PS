ea191d5c2721f27e8a25abaa48cc9eacc32b5021
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-07 16:46:33 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 57.15 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,33 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = new int[2];
+        int[] result = new int[2];
+        
+        Set<String> set = new HashSet<>();
+        Map<String, Integer> countMap = new HashMap<>();
         
         int N = gems.length;
         
-        Map<String, Integer> map = new HashMap<>();
+        for(String gem : gems){
+            set.add(gem);
+        }
         
-        int kind = Arrays.stream(gems).distinct().toArray().length;
+        int kind = set.size();
+        
         int start = 0;
-        int end = 0;
-        int min = 100000;
+        int min = 1000000;
         
-        for(end = 0; end < N; end++){
-            map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
+        for(int i = 0; i < N; i++){
+            countMap.put(gems[i], countMap.getOrDefault(gems[i], 0) + 1);
             
-            while(map.get(gems[start]) > 1){
-                map.put(gems[start], map.get(gems[start]) - 1);
+            while(countMap.get(gems[start]) > 1){
+                countMap.put(gems[start], countMap.get(gems[start]) - 1);
                 start++;
             }
             
-            if(map.size() == kind && min > (end - start)){
-                min = end - start;
-                answer[0] = start + 1;
-                answer[1] = end + 1;
+            if(countMap.size() == kind && min > (i - start)){
+                min = i - start;
+                result[0] = start + 1;
+                result[1] = i + 1;
             }
         }
         
-        
-        return answer;
+        return result;
     }
 }
