20561fcbbf65200766b4f6dfa2d3ac90faba484a
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-12 18:47:18 +0900

    [Gold IV] Title: 수도배관공사, Time: 160 ms, Memory: 15592 KB -BaekjoonHub

--- 백준/Gold/2073. 수도배관공사/수도배관공사.java
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,22 +12,18 @@ public class Main {
         int D = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
 
-        int[] L = new int[P + 1];
-        int[] C = new int[P + 1];
-
-        for(int i = 1; i <= P; i++){
-            st = new StringTokenizer(br.readLine());
-            L[i] = Integer.parseInt(st.nextToken());
-            C[i] = Integer.parseInt(st.nextToken());
-        }
-
         int[] dp = new int[D + 1];
 
         dp[0] = Integer.MAX_VALUE;
         
         for(int i = 1; i <= P; i++){
-            for(int j = D; j >= L[i]; j--){
-                dp[j] = Math.max(dp[j], Math.min(dp[j - L[i]], C[i]));
+            st = new StringTokenizer(br.readLine());
+
+            int L = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            for(int j = D; j >= L; j--){
+                dp[j] = Math.max(dp[j], Math.min(dp[j - L], C));
             }
         }
 
