4f4852653194cbabc1748a1b05fa41e260b90742
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-20 10:17:08 +0900

    [level 3] Title: 상담원 인원, Time: 151.26 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
@@ -1,63 +1,53 @@
 import java.util.*;
 
 class Solution {
-    int K;
+    int[] sizes;
     int N;
+    int K;
     int[][] arr;
     int result = Integer.MAX_VALUE;
-    int[] sizes;
     public int solution(int k, int n, int[][] reqs) {
-        K = k;
+        sizes = new int[k + 1];
         N = n;
+        K = k;
+        
         arr = reqs;
-        
-        
-        sizes = new int[K + 1];
         
         Arrays.fill(sizes, 1);
         
-        dfs(1, K);
+        dfs(1, k);
         
         return result;
     }
     
-    public void dfs(int idx, int count){
-        if(count == N){
+    public void dfs(int idx, int k){
+        if(k == N){
             Map<Integer, PriorityQueue<int[]>> map = new HashMap<>();
             
             for(int i = 1; i <= K; i++){
-                map.put(i, new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]));
+                map.put(i, new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]));
             }
             
             int total = 0;
             
-            // for(int i = 1; i <= K; i++){
-            //     System.out.print(sizes[i] + " ");
-            // }
-            
-            // System.out.println();
-            
             for(int i = 0; i < arr.length; i++){
                 int startTime = arr[i][0];
                 int useTime = arr[i][1];
-                int human = arr[i][2];
+                int type = arr[i][2];
                 
-                if(map.get(human).size() < sizes[human]){
-                    map.get(human).add(new int[]{startTime, startTime + useTime});
+                if(map.get(type).size() < sizes[type]){
+                    map.get(type).add(new int[]{startTime + useTime});
                 } else{
-                    int[] curr = map.get(human).poll();
+                    int[] prev = map.get(type).poll();
                     
-                    // System.out.println(curr[1]);
-                    if(curr[1] <= startTime){
-                        map.get(human).add(new int[]{startTime, startTime + useTime});
+                    if(prev[0] <= startTime){
+                        map.get(type).add(new int[]{startTime + useTime});
                     } else{
-                        total += curr[1] - startTime;
-                        map.get(human).add(new int[]{curr[1], curr[1] + useTime});
+                        total += prev[0] - startTime;
+                        map.get(type).add(new int[]{prev[0] + useTime});
                     }
                 }
             }
-            
-            // System.out.println(total);
             
             result = Math.min(result, total);
             
@@ -66,7 +56,7 @@ class Solution {
         
         for(int i = idx; i <= K; i++){
             sizes[i]++;
-            dfs(i, count + 1);
+            dfs(i, k + 1);
             sizes[i]--;
         }
     }
