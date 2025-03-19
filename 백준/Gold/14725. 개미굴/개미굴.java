e82b0e42b7770c9f95941bd5ddfdb9242b303255
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-19 16:12:54 +0900

    [Gold III] Title: 개미굴, Time: 200 ms, Memory: 16512 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.java
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -1,44 +1,47 @@
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int num = Integer.parseInt(scan.nextLine());
-		
-		Node root = new Node();
-		
-		for(int i = 0; i < num; i++) {
-			String[] arr = scan.nextLine().split(" ");
-			Node cur = root;
-			
-			for(int j = 1; j <= Integer.parseInt(arr[0]); j++) {
-				String s = arr[j];
-				
-				if(!cur.childs.containsKey(s)) {
-					cur.childs.put(s, new Node());
-				}
-				cur = cur.childs.get(s);
-			}
-		}
-		
-		print(root, "");
-	}
-	
-	static void print(Node root, String bar) {
-		Object[] arr = root.childs.keySet().toArray();
-		Arrays.sort(arr);
-		
-		for(Object o : arr) {
-			System.out.println(bar + o);
-			print(root.childs.get(o), bar + "--");
-		}
-	}
-}
+    static class Node {
+        Map<String, Node> childs = new HashMap<>();
+    }
 
-class Node{
-	HashMap<String, Node> childs = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Node root = new Node();
+
+        StringTokenizer st;
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int count = Integer.parseInt(st.nextToken());
+            Node curr = root;
+
+            for(int j = 0; j < count; j++){
+                String s = st.nextToken();
+                if(!curr.childs.containsKey(s)){
+                    curr.childs.put(s, new Node());
+                }
+                curr = curr.childs.get(s);
+            }
+        }
+
+        dfs(root, "");
+    }
+
+    public static void dfs(Node root, String depth){
+        List<String> keys = new ArrayList<>(root.childs.keySet());
+        Collections.sort(keys);
+
+        for(String key : keys){
+            System.out.println(depth + key);
+            dfs(root.childs.get(key), depth + "--");
+        }
+    }
 }
