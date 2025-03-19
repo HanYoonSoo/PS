e1c6c70b15d44cb987b2ce7b4285e324e73c1e93
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-19 13:05:53 +0900

    [Gold II] Title: 8번 출구, Time: 328 ms, Memory: 30972 KB -BaekjoonHub

--- 백준/Gold/33577. 8번 출구/8번 출구.java
+++ 백준/Gold/33577. 8번 출구/8번 출구.java
@@ -1,60 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        long[] immunities = new long[n];
-        
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] arr = new long[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            immunities[i] = Long.parseLong(st.nextToken());
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        long[] prefixSum = new long[n + 1];
-        for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = prefixSum[i] + immunities[i];
+
+        long[] prefixSum = new long[N + 1];
+
+        for(int i = 0; i < N; i++){
+            prefixSum[i + 1] = prefixSum[i] + arr[i];
         }
-        
-        long[] exitsAvailable = new long[n + 1];
-        for (int i = 1; i <= n; i++) {
-            exitsAvailable[i] = immunities[i - 1] - prefixSum[i - 1];
+
+        long[] gain = new long[N + 1];
+
+        for(int i = 1; i <= N; i++){
+            gain[i] = arr[i - 1] - prefixSum[i - 1];
         }
-        
-        long[] exitsMax = new long[n + 1];
-        exitsMax[1] = exitsAvailable[1];
-        for (int i = 2; i <= n; i++) {
-            exitsMax[i] = Math.max(exitsMax[i - 1], exitsAvailable[i]);
+
+        long[] gainMax = new long[N + 1];
+        gainMax[1] = gain[1];
+
+        for(int i = 2; i <= N; i++){
+            gainMax[i] = Math.max(gainMax[i - 1], gain[i]);
         }
-        
-        long immunityCurr = 0;
-        long ret = 0;
-        
-        while (immunityCurr < prefixSum[n]) {
-            int idx = Arrays.binarySearch(prefixSum, immunityCurr);
-            if (idx < 0) {
+
+        long curr = 0;
+        long count = 0;
+
+        while(curr < prefixSum[N]){
+            int idx = Arrays.binarySearch(prefixSum, curr);
+
+            if(idx < 0){
                 idx = -idx - 2;
             }
-            
-            if (idx == n) {
+
+            if(idx == N)
                 break;
-            }
-            
-            long immunityNext = prefixSum[idx + 1];
-            long immunityIncrease = exitsMax[idx + 1];
-            long diff = immunityNext - immunityCurr;
-            
-            long r = (immunityIncrease + diff - 1) / immunityIncrease;
-            immunityCurr += r * immunityIncrease;
-            ret += r;
+
+            long need = prefixSum[idx + 1];
+            long gainValue = gainMax[idx + 1];
+
+            long dist = need - curr;
+
+            long c = (gainValue + dist - 1) / gainValue;
+            curr += c * gainValue;
+            count += c;
         }
-        
-        out.write(ret + "\n");
-        out.flush();
-        br.close();
-        out.close();
+
+        System.out.println(count);
     }
 }
