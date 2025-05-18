794c9f34169a30bc7b0f602befa2694ce82bfcc2
HanYoonSoo <hdbstn3055@gmail.com>
2025-05-18 16:47:09 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 124 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -1,25 +1,35 @@
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(br.readLine());
+        int[] P = new int[N + 1];
+        int[] C = new int[N + 1];
 
-        int N = sc.nextInt();
-        int[] p = new int[1001]; 
-        int[] D = new int[10001];
-        for(int i = 1; i <= N; i++)
-            p[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i = 1; i <= N; i++){
-            D[i] = p[i];
-            for(int j = 1; j <= i; j++){
-                D[i] = Math.min(D[i], D[i-j]+p[j]);
+            P[i] = Integer.parseInt(st.nextToken());
+            C[i] = i;
+        }
+
+        int[] dp = new int[N + 1];
+
+        Arrays.fill(dp, 987654321);
+        dp[0] = 0;
+
+        for(int i = 1; i <= N; i++){
+            for(int j = C[i]; j <= N; j++){
+                dp[j] = Math.min(dp[j], dp[j - C[i]] + P[i]);
             }
         }
 
-        System.out.println(D[N]);
-        sc.close();
+        System.out.println(dp[N]);
     }
 }
