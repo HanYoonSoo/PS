95244eff03ec10e20130879bba3e40d305a08d76
HanYoonSoo <hdbstn3055@gmail.com>
2025-11-04 21:05:48 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 84.5 MB, 시간: 0.55 ms
+메모리: 82.4 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 23일 14:51:18
+2025년 11월 04일 21:05:45
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 class Solution {
-    int[] parent;
     int N;
+    int[] parent;
     public int solution(int n, int[][] computers) {
-        
         N = n;
         parent = new int[N];
         
@@ -14,20 +13,20 @@ class Solution {
         
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
-                if(i != j){
-                    if(computers[i][j] == 1){
+                if(i != j) {
+                    if(computers[i][j] == 1) {
                         union(i, j);
                     }
                 }
-            }    
+            }
         }
         
-        boolean[] visited = new boolean[200];
-        
+        boolean[] visited = new boolean[N];
         int count = 0;
-        for(int i = 0; i < N; i++){
-            if(!visited[find_parent(parent[i])]){
-                visited[find_parent(parent[i])] = true;
+        
+        for(int i = 0; i < N; i++) {
+            if(!visited[find_parent(i)]) {
+                visited[i] = true;
                 count++;
             }
         }
@@ -35,24 +34,24 @@ class Solution {
         return count;
     }
     
-    public int find_parent(int a){
-        if(parent[a] == a){
-            return a;
+    public void union(int a, int b) {
+        a = find_parent(a);
+        b = find_parent(b);
+        
+        if(a < b) {
+            parent[b] = a;
+        } else {
+            parent[a] = b;
+        }
+    }
+    
+    public int find_parent(int a) {
+        if(parent[a] == a) {
+            return parent[a];
         }
         
         parent[a] = find_parent(parent[a]);
         
         return parent[a];
-    }
-    
-    public void union(int a, int b){
-        a = find_parent(a);
-        b = find_parent(b);
-        
-        if(a <= b){
-            parent[b] = a;
-        } else{
-            parent[a] = b;
-        }
     }
 }
