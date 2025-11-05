09170c371922436842ec1ad51e301b2e7146eddd
HanYoonSoo <hdbstn3055@gmail.com>
2025-11-05 21:48:20 +0900

    [level 3] Title: 야근 지수, Time: 129.62 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -4,21 +4,20 @@ class Solution {
     public long solution(int n, int[] works) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
         
-        for(int work : works){
-            pq.add(work);
+        for(int w : works) {
+            pq.add(w);
         }
         
-        for(int i = 0; i < n; i++){
-            if(!pq.isEmpty()){
-                int num = pq.poll();
-                if(num > 1)
-                    pq.add(num - 1);
-            }
+        for(int i = 0; i < n; i++) {
+            int biggest = pq.poll();
+            biggest--;
+            
+            pq.add(Math.max(0, biggest));
         }
         
         long result = 0;
         
-        while(!pq.isEmpty()){
+        while(!pq.isEmpty()) {
             int num = pq.poll();
             result += num * num;
         }
