12fb3cfebffb706be6086908524b4145c9e2682b
HanYoonSoo <hdbstn3055@gmail.com>
2025-11-05 22:43:02 +0900

    [level 3] Title: 단속카메라, Time: 8.98 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,13 +1,9 @@
 import java.util.*;
 
 class Solution {
-    
-    // 스케줄링 문제 같음
-    // 모든 차량이 한번은 단속용 카메라를 만나도록 하려면 최소 몇 대의 카메라를 설치?
-    // routes 이동하는 차량의 경로
     public int solution(int[][] routes) {
         Arrays.sort(routes, (o1, o2) -> {
-            if(o1[1] == o2[1]){
+            if(o1[1] == o2[1]) {
                 return o1[0] - o2[0];
             }
             return o1[1] - o2[1];
@@ -16,8 +12,8 @@ class Solution {
         int end = routes[0][1];
         int count = 1;
         
-        for(int i = 1; i < routes.length; i++){
-            if(end < routes[i][0]){
+        for(int i = 1; i < routes.length; i++) {
+            if(end < routes[i][0]) {
                 end = routes[i][1];
                 count++;
             }
