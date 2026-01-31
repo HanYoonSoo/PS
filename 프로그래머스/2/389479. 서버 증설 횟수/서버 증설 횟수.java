76ecbff6484616319821109041b9706b8c8e61f1
HanYoonSoo <hdbstn3055@gmail.com>
2026-01-31 16:09:18 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.13 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,31 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] players, int m, int k) {
-        int[] addServer = new int[24];
-    
-        int curr = m;
-        int answer = 0;
+        Queue<int[]> q = new LinkedList<>();
         
-        for(int i = 0; i < 24; i++){
-            if(addServer[i] != 0){
-                curr -= addServer[i];
-            }
-            
-            int uses = 0;
-            uses = players[i];
-            
-            if(uses >= curr){
-                int compute = (uses - curr) / m + 1;
-                answer += compute;
-                
-                if(i + k < 24){
-                    addServer[i + k] += compute * m;
+        int maxHuman = m;
+        int update = 0;
+        
+        for (int i = 0; i < players.length; i++) {
+            if (!q.isEmpty()) {
+                if(q.peek()[0] == i) {
+                    maxHuman -= q.poll()[1];
                 }
-                
-                curr += compute * m;
-                // System.out.println(i + " " + compute);
+            }
+            int currentUser = players[i];
+            
+            if (maxHuman <= currentUser) {
+                int need = (currentUser - maxHuman) / m + 1;
+                update += need;
+                maxHuman += need * m;
+                q.add(new int[]{i + k, need * m});
             }
         }
         
-        return answer;
+        return update;
     }
 }
